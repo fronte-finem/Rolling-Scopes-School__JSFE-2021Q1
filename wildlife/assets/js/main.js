82ab2f7e8d6645a09f1ffa64938b1749d37fca76
fronte-finem <fronte.finem+github@ya.ru>
2021-03-15 23:27:36 +0300

    fix: wait page loading before run js handlers

--- wildlife/assets/js/main.js
+++ wildlife/assets/js/main.js
@@ -2,10 +2,10 @@ import { Carousel } from './block/carousel.js'
 import { cards } from './cfg/cards.js'
 
 
-let carousel = new Carousel(document.querySelector('.carousel'));
+window.addEventListener('load', (e) => {
+  let carousel = new Carousel(document.querySelector('.carousel'));
+  // console.log(carousel);
+  cards.concat(cards).concat(cards).forEach(carousel.addCard.bind(carousel));
+  carousel.changeSlot(3);
+});
 
-cards.concat(cards).concat(cards).forEach(carousel.addCard.bind(carousel));
-
-carousel.changeSlot(3);
-
-// console.log(carousel);
