bdd67eca6abf4bcdcb4e2193160edefb89d084aa
fronte-finem <fronte.finem+github@ya.ru>
2021-03-16 00:00:04 +0300

    fix: for easy cross-checking add back carousel markup

--- wildlife/assets/js/main.js
+++ wildlife/assets/js/main.js
@@ -5,7 +5,7 @@ import { cards } from './cfg/cards.js'
 window.addEventListener('load', (e) => {
   let carousel = new Carousel(document.querySelector('.carousel'));
   // console.log(carousel);
-  cards.concat(cards).concat(cards).forEach(carousel.addCard.bind(carousel));
+  cards.concat(cards).forEach(carousel.addCard.bind(carousel));
   carousel.changeSlot(3);
 });
 
