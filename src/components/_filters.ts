a72aa0b2cd5976a33493e3438ef0cc3451a76a07
fronte-finem <fronte.finem+github@ya.ru>
2021-04-24 16:45:38 +0300

    refactor: add observer in filters container

--- src/components/_filters.ts
+++ src/components/_filters.ts
@@ -1,18 +1,24 @@
 import { newElem } from '../lib/dom-helpers.js';
-import { FilterIOView, FilterIOViewSettings } from './_filter-io.js';
+import { observer } from '../lib/observer.js';
+import { CssVar } from '../lib/types.js';
+import { FilterIO, FilterIOSettings } from './_filter-io.js';
 
-export { FiltersView };
+export { Filters };
 
-class FiltersView {
+class Filters {
+  static viewName = 'filters';
+
   view: HTMLUListElement;
 
-  constructor (settings: FilterIOViewSettings[]) {
-    this.view = newElem('ul', 'filters') as HTMLUListElement;
-    this.view.append(...settings.map(st => FiltersView.wrapper(new FilterIOView(st).view)));
+  constructor (settings: FilterIOSettings[]) {
+    this.view = newElem('ul', Filters.viewName) as HTMLUListElement;
+    this.view.append(...settings.map(st => Filters.wrapper(new FilterIO(st).view)));
+
+    observer.sub(FilterIO.viewName, (cssVar: CssVar) => observer.fire(Filters.viewName, cssVar));
   }
 
   static wrapper (el: HTMLElement): HTMLLIElement {
-    const li = newElem('li', 'filters__item') as HTMLLIElement;
+    const li = newElem('li', `${Filters.viewName}__item`) as HTMLLIElement;
     li.append(el);
     return li;
   }
