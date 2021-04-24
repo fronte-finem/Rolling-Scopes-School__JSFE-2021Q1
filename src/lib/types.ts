6601317e956af867148ebcd73f24ca62340e57b1
fronte-finem <fronte.finem+github@ya.ru>
2021-04-24 21:22:18 +0300

    feat: add img links generator

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -14,3 +14,24 @@ export abstract class ViewBEM {
     return [elem, ...mods.map(mod => `${elem}--${mod}`)].join(' ');
   }
 }
+
+export class ImageLinksRoll {
+  static init = './assets/img/img.jpg';
+  static base = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images';
+  static periods = ['night', 'morning', 'day', 'evening'];
+
+  counter: number;
+
+  constructor() {
+    this.counter = 0;
+  }
+
+  get init() { return ImageLinksRoll.init; }
+
+  next(date?: Date): string {
+    const num = String(1 + this.counter++ % 20).padStart(2, '0');
+    const i = Math.floor((date ?? new Date()).getHours() / 6);
+    const period = ImageLinksRoll.periods[i];
+    return `${ImageLinksRoll.base}/${period}/${num}.jpg`;
+  }
+}
