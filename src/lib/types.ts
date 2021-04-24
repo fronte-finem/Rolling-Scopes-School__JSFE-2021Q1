f3dac79919ff7f39e5930d2e3e8b5bca3a2536e1
fronte-finem <fronte.finem+github@ya.ru>
2021-04-24 17:56:03 +0300

    feat: add abstract class for bem namings

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,4 +1,19 @@
+import { close } from "node:inspector";
+
 export type CssVar = {
   name: string;
   value: string;
+}
+
+type Class<T> = new (...args: any[]) => T;
+
+export abstract class ViewBEM {
+  static ViewName: string;
+
+  static bem<T extends ViewBEM>(this: Class<T>, element?: string, modificator?: string | string[]): string {
+    const self = this as unknown as typeof ViewBEM;
+    const mods = Array.isArray(modificator) ? modificator : (modificator ? [modificator] : []);
+    const elem = element ? `${self.ViewName}__${element}` : self.ViewName;
+    return [elem, ...mods.map(mod => `${elem}--${mod}`)].join(' ');
+  }
 }
