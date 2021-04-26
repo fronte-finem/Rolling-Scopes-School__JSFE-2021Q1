88511d7c96e834bc7dcaddcb18744ddc9edbb198
fronte-finem <fronte.finem+github@ya.ru>
2021-04-26 23:30:30 +0300

    refactor: use buttons components

--- src/components/_filters.ts
+++ src/components/_filters.ts
@@ -1,6 +1,7 @@
-import { newElem } from '../lib/dom-helpers.js';
+import { newElem, newDiv, htmlToElem } from '../lib/dom-helpers.js';
 import { observer } from '../lib/observer.js';
 import { ViewBEM, CssVar, CssFilter, CssFilters } from '../lib/types.js';
+import { Btn } from './_btn.js';
 import { FilterIO, FilterIOSettings } from './_filter-io.js';
 
 export { Filters };
@@ -8,12 +9,20 @@ export { Filters };
 class Filters extends ViewBEM {
   static ViewName = 'filters';
 
-  view: HTMLUListElement;
+  view: HTMLElement;
 
   constructor (settings: FilterIOSettings[]) {
     super();
-    this.view = newElem('ul', Filters.ViewName) as HTMLUListElement;
-    this.view.append(...settings.map(st => Filters.wrapper(new FilterIO(st).view)));
+
+    this.view = newDiv(Filters.ViewName);
+
+    const btnCont = newDiv(Filters.bem('container', 'btn'));
+    btnCont.append(this.initBtnReset('Reset'));
+
+    const filters = newElem('ul', Filters.bem('items')) as HTMLUListElement;
+    filters.append(...settings.map(st => Filters.wrapper(new FilterIO(st).view)));
+
+    this.view.append(btnCont, filters);
 
     observer.sub(FilterIO.ViewName, (cssVar: CssVar) => observer.fire(Filters.ViewName, cssVar));
 
@@ -27,6 +36,12 @@ class Filters extends ViewBEM {
       }, {} as CssFilters);
       observer.fire(`${Filters.ViewName}:filter`, filters);
     });
+  }
+
+  initBtnReset(text: string) {
+    const btnReset = new Btn('reset', text).view;
+    btnReset.addEventListener('click', () => observer.fire(`${Filters.ViewName}:reset`));
+    return btnReset;
   }
 
   static wrapper (el: HTMLElement): HTMLLIElement {
