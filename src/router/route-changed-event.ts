f0a87b994a99fc12e579087ad5f773dadeac6f4a
fronte-finem <fronte.finem+github@ya.ru>
2021-05-17 00:39:50 +0300

    refactor: simplify router

--- src/router/route-changed-event.ts
+++
@@ -1,8 +0,0 @@
-import { IPageView } from '../pages/base-page';
-import { IEvent } from '../shared/observer';
-
-export default class RouteChangedEvent implements IEvent<IPageView> {
-  constructor(readonly data: IPageView) {
-    this.data = data;
-  }
-}
