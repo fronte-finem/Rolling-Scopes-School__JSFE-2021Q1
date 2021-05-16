47e341f952c11dcb669c23d906d7763eedd8aec4
fronte-finem <fronte.finem+github@ya.ru>
2021-05-17 00:42:06 +0300

    feat: add card component

---
+++ src/components/card/card-model.ts
@@ -0,0 +1,55 @@
+import { State } from '../../shared/types';
+import Model from '../../shared/models/model';
+import delay from '../../shared/timer-utils';
+
+const ERROR_TIME = 2;
+
+export interface ICardModelState extends State {
+    isError: boolean;
+    isMatch: boolean;
+    isFrontSide: boolean;
+    clickedCount: number;
+    errorCount: number;
+}
+
+export default class CardModel extends Model<ICardModelState> {
+  constructor(
+    readonly id: number,
+    readonly frontImage: string,
+    readonly backImage: string
+  ) {
+    super({
+      isError: false,
+      isMatch: false,
+      isFrontSide: false,
+      clickedCount: 0,
+      errorCount: 0,
+    });
+  }
+
+  flip(toFront: boolean): void {
+    if (this.state.isMatch || this.state.isError) return;
+    this.state.isFrontSide = toFront;
+  }
+
+  match(isMatch: boolean): void {
+    this.state.isMatch = isMatch;
+  }
+
+  async error(): Promise<void> {
+    if (this.state.isMatch) return;
+    this.state.errorCount += 1;
+    this.state.isError = true;
+    await delay(ERROR_TIME * 1000);
+    this.state.isError = false;
+    this.flip(false);
+  }
+
+  click(): void {
+    if (this.state.isMatch || this.state.isError || this.state.isFrontSide) {
+      return;
+    }
+    this.flip(true);
+    this.state.clickedCount += 1;
+  }
+}
