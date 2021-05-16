b2d8075f3756da4fb9d7ce27757079aa0c907204
fronte-finem <fronte.finem+github@ya.ru>
2021-05-17 00:36:48 +0300

    feat: add abstract model type

---
+++ src/shared/models/model.ts
@@ -0,0 +1,45 @@
+import Observer, { Listener } from "../observer";
+import { State } from "../types";
+
+export default abstract class Model<S extends State> {
+  protected readonly observer = new Observer();
+
+  protected readonly state: S;
+
+  constructor(state: S) {
+    this.state = this.proxify(state);
+  }
+
+  getState(): S {
+    return { ...this.state };
+  }
+
+  protected stateChanged?: (state: S) => void;
+
+  onStateChange(listener: Listener<S>): void {
+    this.observer.subscribe('state-change', listener);
+  }
+
+  protected proxify(state: S): S {
+    return new Proxy(state, {
+      set: (target, prop: string, value: S[string]) => {
+        const st = target;
+        // Model.logChangeState(target, prop, value);
+        (st[prop] as S[string]) = value;
+        this.observer.notify('state-change', this.getState());
+        this.observer.notify(prop, value);
+        return true;
+      },
+    });
+  }
+
+  static logChangeState<T extends State>(
+    target: T,
+    prop: string,
+    value: T[string]
+  ): string {
+    const msg = `Changin ${target} property ${prop} from ${target[prop]} to ${value}`;
+    // console.log(msg);
+    return msg;
+  }
+}
