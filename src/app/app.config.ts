cbacd6203a8ae3b93f9b25ee22157d9537ce82e6
fronte-finem <fronte.finem+github@ya.ru>
2021-05-20 19:56:10 +0300

    refactor: DRY app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,25 +1,66 @@
-export const appConfig = {
+import { PageAbout, PageGame, PageScore, PageSettings } from '../pages/index';
+import { IRoute } from '../router/router';
+import { CardImagesService } from '../services/card-images-urls';
+
+export interface IAppConfig {
+  readonly initialRoute: IRoute;
+  readonly pages: {
+    readonly about: IPageConfig;
+    readonly score: IPageConfig;
+    readonly settings: IPageConfig;
+    readonly game: IPageConfig;
+  };
+  readonly header: IHeaderConfig;
+}
+
+export interface IPageConfig {
+  readonly title: string;
+  readonly route: IRoute;
+  readonly navSvgIcon?: string;
+}
+
+export interface IHeaderConfig {
+  readonly btn: {
+    readonly signUp: string;
+    readonly start: string;
+    readonly stop: string;
+  };
+}
+
+const cardImagesService = new CardImagesService();
+
+const initialRoute: Readonly<IRoute> = {
+  url: '#/about-game',
+  pageCreator: () => new PageAbout(),
+};
+
+export const APP_CONFIG: Readonly<IAppConfig> = {
+  initialRoute,
+  pages: {
+    about: {
+      title: 'about game',
+      route: { url: '#/about-game', pageCreator: () => new PageScore() },
+      navSvgIcon: './svg/sprite.svg#icon-question-mark',
+    },
+    score: {
+      title: 'best score',
+      route: { url: '#/best-score', pageCreator: () => new PageScore() },
+      navSvgIcon: './svg/sprite.svg#icon-star',
+    },
+    settings: {
+      title: 'game settings',
+      route: { url: '#/game-settings', pageCreator: () => new PageSettings() },
+      navSvgIcon: './svg/sprite.svg#icon-gear',
+    },
+    game: {
+      title: 'about game',
+      route: {
+        url: '#/game',
+        pageCreator: () => new PageGame(cardImagesService),
+      },
+    },
+  },
   header: {
-    navMenu: [
-      {
-        page: 'game',
-        title: 'about game',
-        url: '#/about-game',
-        svgIconLink: './svg/sprite.svg#icon-question-mark',
-      },
-      {
-        page: 'score',
-        title: 'best score',
-        url: '#/best-score',
-        svgIconLink: './svg/sprite.svg#icon-star',
-      },
-      {
-        page: 'settings',
-        title: 'game settings',
-        url: '#/game-settings',
-        svgIconLink: './svg/sprite.svg#icon-gear',
-      },
-    ],
     btn: {
       signUp: 'register new player',
       start: 'start game',
