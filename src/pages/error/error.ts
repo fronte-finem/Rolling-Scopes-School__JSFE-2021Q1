b79e4f202d902b73d963fc609ede552bdb3ec6a8
fronte-finem <fronte.finem+github@ya.ru>
2021-05-21 23:11:36 +0300

    refactor: simplify base page after reworked app-config

--- src/pages/error/error.ts
+++ src/pages/error/error.ts
@@ -1,9 +1,7 @@
 import { BasePage } from '../base-page';
 
-const PAGE_TITLE = 'error 404';
-
 export class PageError extends BasePage {
   constructor() {
-    super(PAGE_TITLE, { childs: [{ tag: 'h1', text: PAGE_TITLE }] });
+    super({ childs: [{ tag: 'h1', text: PageError.name }] });
   }
 }
