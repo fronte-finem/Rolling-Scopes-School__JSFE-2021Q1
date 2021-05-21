a9f77b5163ace8d51f83620b82c45894a3d65c78
fronte-finem <fronte.finem+github@ya.ru>
2021-05-21 23:23:09 +0300

    feat: add typings for game model and settings

---
+++ src/pages/game/game-types.ts
@@ -0,0 +1,29 @@
+import { CardImagesCategory } from '../../services/card-images-urls';
+
+export type CardField = readonly [columns: number, rows: number];
+
+export type GameDifficulty = {
+  readonly cardField: CardField;
+  readonly initialShowTime: number;
+  readonly scoreCoefficient: number;
+};
+
+export type GameDifficultyMap = Readonly<{
+  12: GameDifficulty;
+  16: GameDifficulty;
+  20: GameDifficulty;
+  24: GameDifficulty;
+  30: GameDifficulty;
+  36: GameDifficulty;
+  42: GameDifficulty;
+  48: GameDifficulty;
+  56: GameDifficulty;
+  64: GameDifficulty;
+}>;
+
+export type CardsAmount = keyof GameDifficultyMap;
+
+export interface IGameSettings {
+  cardImagesCategory: keyof typeof CardImagesCategory;
+  cardsAmount: keyof GameDifficultyMap;
+}
