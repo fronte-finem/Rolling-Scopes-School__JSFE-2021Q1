da646fa98ed9cf27f3f2b2977abab9008c385286
fronte-finem <fronte.finem+github@ya.ru>
2021-05-21 22:51:39 +0300

    refactor: change type casting form

--- src/shared/observer-lite.ts
+++ src/shared/observer-lite.ts
@@ -4,11 +4,11 @@ export class ObserverLite {
   private readonly listeners = new Set<Listener<unknown>>();
 
   subscribe<T>(listener: Listener<T>): void {
-    this.listeners.add(<Listener<unknown>>listener);
+    this.listeners.add(listener as Listener<unknown>);
   }
 
   unsubscribe<T>(listener: Listener<T>): void {
-    this.listeners.delete(<Listener<unknown>>listener);
+    this.listeners.delete(listener as Listener<unknown>);
   }
 
   notify<T>(data: T): void {
