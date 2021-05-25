88ab0fbdb5839e9428cc54f434c32599475363ae
fronte-finem <fronte.finem+github@ya.ru>
2021-05-25 15:17:37 +0300

    feat: create congrats pop-up view

---
+++ src/components/pop-up-victory/pop-up-victory-view.ts
@@ -0,0 +1,50 @@
+import styles from './pop-up-victory-view.scss';
+import {
+  APP_POPUP_VICTORY_CONFIG,
+  PopUpVictoryBtns,
+} from '../../app/configs/popups';
+import { BtnView } from '../../shared/views/btn/btn';
+import { PopUpView } from '../pop-up/pop-up-view';
+import { IUser, IUserService } from '../../services/user-service';
+import { View } from '../../shared/views/view';
+import { timeDiff } from '../../shared/date-time-utils';
+
+const createBtn = (key: PopUpVictoryBtns, ...classNames: string[]): BtnView =>
+  new BtnView({ ...APP_POPUP_VICTORY_CONFIG.btns[key], classNames });
+
+const msgGenerator = (user: IUser) => {
+  const { hours, min, sec } = timeDiff(user.time);
+  let timeFormat = [`${hours} hours`, `${min} minutes`, `${sec} seconds`];
+  timeFormat = user.time > 3600 ? timeFormat : timeFormat.slice(1);
+  timeFormat = user.time > 60 ? timeFormat : timeFormat.slice(1);
+  return new View(
+    `<div class="${styles.output}">Congratulations,
+    <span class="${styles.highlight}">${user.firstName} ${user.lastName}</span>!
+    <br>You successfully found all matches in time
+    <span class="${styles.highlight}">${timeFormat.join(' ')}</span>
+    <br>with score <span class="${styles.highlight}">${
+      user.score
+    }</span>.</div>`
+  );
+};
+
+export class PopUpVictoryView extends PopUpView {
+  btnConfirn = createBtn('confirm');
+
+  constructor(private readonly userService: IUserService, time = NaN) {
+    super(APP_POPUP_VICTORY_CONFIG.title);
+    this.init(time, this.userService.currentUser);
+  }
+
+  private init(time: number, user: IUser | undefined) {
+    if (!user) return;
+    this.addContent(msgGenerator(user));
+    this.addButtons(this.btnConfirn);
+  }
+
+  task(): Promise<boolean> {
+    return new Promise((resolve) => {
+      this.btnConfirn.onClick(() => resolve(true));
+    });
+  }
+}
