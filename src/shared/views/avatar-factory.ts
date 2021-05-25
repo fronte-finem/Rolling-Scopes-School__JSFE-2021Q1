d68eebfda18457dc9bf10ea137872a260cd59794
fronte-finem <fronte.finem+github@ya.ru>
2021-05-25 14:46:27 +0300

    feat: add avatar factory function

---
+++ src/shared/views/avatar-factory.ts
@@ -0,0 +1,23 @@
+import { IUser } from '../../services/user-service';
+import { createSvgSpriteElement } from '../dom-utils';
+import { ImgView } from './img/img';
+import { View } from './view';
+
+const DEFAULT_AVATAR_SVG_ICON = './svg/sprite.svg#icon-avatar';
+
+export function renderAvatar(
+  user: IUser,
+  avatarContainer: View,
+  svgClassNames: string[],
+  imgClassNames: string[]
+): void {
+  if (!user.avatar) {
+    avatarContainer.element.append(
+      createSvgSpriteElement(DEFAULT_AVATAR_SVG_ICON, svgClassNames)
+    );
+  } else {
+    avatarContainer.render(
+      new ImgView({ url: user.avatar, classNames: imgClassNames })
+    );
+  }
+}
