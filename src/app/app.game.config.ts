13e361b815975720d0ca13ee9efd2f69406bf3d2
fronte-finem <fronte.finem+github@ya.ru>
2021-05-26 23:03:13 +0300

    refactor: add cards-field model

--- src/app/app.game.config.ts
+++ src/app/app.game.config.ts
@@ -1,26 +1,43 @@
-import { GameDifficultyMap, IGameSettings } from '../pages/game/game-types';
+import { CardFieldModel } from '../components/cards-field/card-field-model';
+import { IGameSettings } from '../pages/game/game-types';
 import { CardImagesCategory } from '../services/card-images-urls';
 
 export interface IAppGameConfig {
   readonly settings: Readonly<IGameSettings>;
 }
 
+export const APP_GAME_CARD_FIELDS: CardFieldModel[] = [
+  new CardFieldModel(4, 3),
+  new CardFieldModel(4, 4),
+  new CardFieldModel(5, 4),
+  new CardFieldModel(6, 4),
+  new CardFieldModel(6, 5),
+  new CardFieldModel(6, 6),
+  new CardFieldModel(7, 6),
+  new CardFieldModel(8, 6),
+  new CardFieldModel(8, 7),
+  new CardFieldModel(8, 8),
+];
+
+export const APP_GAME_INITIAL_SHOW_TIME: number[] = Array.from(
+  {
+    length: 6,
+  },
+  (_, i) => 5 * (i + 1)
+);
+
+export const APP_GAME_MISMATCH_SHOW_TIME: number[] = Array.from(
+  {
+    length: 5,
+  },
+  (_, i) => i + 1
+);
+
 export const APP_GAME_INITIAL_CONFIG: Readonly<IAppGameConfig> = {
   settings: {
     cardImagesCategory: CardImagesCategory.DOGS,
-    cardsAmount: 20,
+    cardsField: APP_GAME_CARD_FIELDS[5],
+    initialShowTime: APP_GAME_INITIAL_SHOW_TIME[1],
+    mismatchShowTime: APP_GAME_MISMATCH_SHOW_TIME[2],
   },
 };
-
-export const APP_GAME_DIFFICILTY_CONFIG: GameDifficultyMap = {
-  12: { cardField: [4, 3], initialShowTime: 3, scoreCoefficient: 1.0 },
-  16: { cardField: [4, 4], initialShowTime: 4, scoreCoefficient: 1.1 },
-  20: { cardField: [5, 4], initialShowTime: 5, scoreCoefficient: 1.2 },
-  24: { cardField: [6, 4], initialShowTime: 6, scoreCoefficient: 1.4 },
-  30: { cardField: [6, 5], initialShowTime: 8, scoreCoefficient: 1.7 },
-  36: { cardField: [6, 6], initialShowTime: 10, scoreCoefficient: 2.0 },
-  42: { cardField: [7, 6], initialShowTime: 12, scoreCoefficient: 2.5 },
-  48: { cardField: [8, 6], initialShowTime: 15, scoreCoefficient: 3.0 },
-  56: { cardField: [8, 7], initialShowTime: 20, scoreCoefficient: 3.5 },
-  64: { cardField: [8, 8], initialShowTime: 25, scoreCoefficient: 4.0 },
-} as const;
