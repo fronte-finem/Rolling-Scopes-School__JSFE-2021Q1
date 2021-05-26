4ad59ba3e49a398bdf3a5d12fb106ab36328eb58
fronte-finem <fronte.finem+github@ya.ru>
2021-05-26 14:48:46 +0300

    refactor: simplify select-view

--- src/components/select/select-view.ts
+++ src/components/select/select-view.ts
@@ -1,82 +1,58 @@
 import styles from './select-view.scss';
 import { ICreateViewOptions, View } from '../../shared/views/view';
-import { ISelectOpionModelState } from './select-model';
+import { OptionView } from './option-view';
 
-export interface ISelectOption<T> {
-  value: T;
-  text: string;
-}
 export interface ICreateSelectOptions extends ICreateViewOptions {
   heading: string;
   placeholder: string;
 }
 
+export interface ICreateOptionOptions<T> {
+  value: T;
+  text: string;
+  selected?: boolean;
+  disabled?: boolean;
+  title?: string;
+}
+
 export class SelectView<T extends string = string> extends View {
   private readonly optionsMap = new Map<string, T>();
 
-  readonly label = new View<HTMLLabelElement>({
+  private readonly label = new View<HTMLLabelElement>({
     tag: 'label',
     classNames: [styles.selectLabel],
   });
 
-  readonly select = new View<HTMLSelectElement>({
+  private readonly select = new View<HTMLSelectElement>({
     tag: 'select',
     classNames: [styles.selector],
   });
 
-  readonly placeholder = SelectView.createOption({
+  private readonly placeholder = new OptionView({
     value: 'placeholder',
     text: 'placeholder',
     selected: true,
     disabled: true,
   });
 
-  constructor({
-    heading,
-    placeholder,
-    classNames,
-    ...options
-  }: ICreateSelectOptions) {
-    super({
-      ...options,
-      classNames: [styles.selectContainer].concat(classNames || []),
-    });
+  public constructor({ classNames = [], ...options }: ICreateSelectOptions) {
+    super({ ...options, classNames: [...classNames, styles.selectContainer] });
+    this.init(options);
+  }
 
+  private init({ heading, placeholder }: ICreateSelectOptions): void {
     this.render([this.label, this.select]);
     this.label.setText(heading);
     this.placeholder.setText(placeholder);
-    this.select.render(this.placeholder);
+    this.select.element.add(this.placeholder.element);
   }
 
-  addOptions(opts: ISelectOpionModelState<T>[]): SelectView {
-    opts.forEach((opt) => this.optionsMap.set(opt.value, opt.value));
-    this.select.render([
-      this.placeholder,
-      ...opts.map((opt) => SelectView.createOption(opt)),
-    ]);
+  public addOptions(opts: ICreateOptionOptions<T>[]): SelectView {
+    opts.map((opt) => this.select.element.add(new OptionView(opt).element));
     return this;
   }
 
-  static createOption<V extends string = string>({
-    value,
-    text,
-    selected,
-    disabled,
-    title,
-  }: ISelectOpionModelState<V>): View<HTMLOptionElement> {
-    const option = new View<HTMLOptionElement>({
-      tag: 'option',
-      classNames: [styles.option],
-    });
-    option.element.setAttribute('value', value);
-    option.setText(text);
-    if (selected) option.element.selected = true;
-    if (disabled) option.element.disabled = true;
-    if (title) option.element.title = title;
-    return option;
-  }
-
-  onSelect(
+  public onSelect(
     listener: (value: T) => void,
     options?: boolean | AddEventListenerOptions
   ): void {
