91f07f7947076e1b2561b0971ff7c3f25c560b6f
fronte-finem <fronte.finem+github@ya.ru>
2021-05-26 23:12:19 +0300

    refactor: use new range-view for detailed settings

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,4 +1,8 @@
-import { APP_GAME_DIFFICILTY_CONFIG as APP_GAME_DIFFICULTY_CONFIG } from '../../app/app.game.config';
+import {
+  APP_GAME_CARD_FIELDS,
+  APP_GAME_INITIAL_SHOW_TIME,
+  APP_GAME_MISMATCH_SHOW_TIME,
+} from '../../app/app.game.config';
 import {
   CardImagesCategory,
   CARD_IMAGES_CATEGORY_TEXT_MAP,
@@ -8,23 +12,37 @@ import { View } from '../../shared/views/view';
 import { SelectView } from '../../components/select/select-view';
 import { BasePage } from '../base-page';
 import { GameSettingsModel, IGameSettingsState } from './settings-model';
-import { CardsAmount } from '../game/game-types';
 import styles from './settings.scss';
+import { InputRangeView } from '../../components/range-input/range-input-view';
 
 export class PageSettings extends BasePage {
   private model = new GameSettingsModel();
 
   selectGameCards = new SelectView<CardImagesCategory>({
     heading: 'game cards',
-    placeholder: 'select game cards type',
+    placeholder: 'Select game cards type',
     classNames: [styles.settingsSelect],
   });
 
-  selectDifficulty = new SelectView({
-    heading: 'difficulty',
-    placeholder: '🎴 cards \xa0|\xa0 ⏱ start |\xa0 🏆 score',
-    classNames: [styles.settingsSelect],
+  cardsRange = new InputRangeView({
+    title: '🎴🎴 Cards field (rows × columns)',
+    values: APP_GAME_CARD_FIELDS,
+    classNames: [styles.range],
   });
+
+  initialShowTimeRange = new InputRangeView({
+    title: '⏱🃏 Start game countdown (seconds)',
+    values: APP_GAME_INITIAL_SHOW_TIME,
+    classNames: [styles.range],
+  });
+
+  mismatchShowTimeRange = new InputRangeView({
+    title: '🍎🍏 Delay flip after mismatch (seconds)',
+    values: APP_GAME_MISMATCH_SHOW_TIME,
+    classNames: [styles.range],
+  });
+
+  wrapper = new View({ classNames: [styles.settingsWrapper] });
 
   constructor(private gameSettingsService: IGameSettingsService) {
     super({ classNames: [styles.settings] });
@@ -35,12 +53,13 @@ export class PageSettings extends BasePage {
   }
 
   init(): void {
-    this.view.render(
-      new View({
-        classNames: [styles.settingsWrapper],
-        childs: [this.selectDifficulty, this.selectGameCards],
-      })
-    );
+    this.view.render(this.wrapper);
+    this.wrapper.render([
+      this.selectGameCards,
+      this.cardsRange,
+      this.initialShowTimeRange,
+      this.mismatchShowTimeRange,
+    ]);
 
     const initialSettings = this.gameSettingsService.loadSettings();
 
@@ -49,21 +68,30 @@ export class PageSettings extends BasePage {
       this.gameSettingsService.saveSettings(settings)
     );
     this.initSelectsors(this.model.getState());
+    this.initRanges(this.model.getState());
+  }
+
+  private initRanges(state: IGameSettingsState): void {
+    this.cardsRange.setValue(state.cardsField);
+    this.initialShowTimeRange.setValue(state.initialShowTime);
+    this.mismatchShowTimeRange.setValue(state.mismatchShowTime);
+
+    this.cardsRange.onSelect((value) => {
+      this.model.state.cardsField = value;
+    });
+    this.initialShowTimeRange.onSelect((value) => {
+      this.model.state.initialShowTime = value;
+    });
+    this.mismatchShowTimeRange.onSelect((value) => {
+      this.model.state.mismatchShowTime = value;
+    });
   }
 
   private initSelectsors(state: IGameSettingsState): void {
     this.selectGameCards.addOptions(PageSettings.prepareCategoryOptions(state));
 
-    this.selectDifficulty.addOptions(
-      PageSettings.prepareDifficultyOptions(state)
-    );
-
     this.selectGameCards.onSelect((value) => {
       this.model.state.cardImagesCategory = value;
-    });
-
-    this.selectDifficulty.onSelect((value) => {
-      this.model.state.cardsAmount = value as unknown as CardsAmount;
     });
   }
 
@@ -73,20 +101,6 @@ export class PageSettings extends BasePage {
       text: CARD_IMAGES_CATEGORY_TEXT_MAP.get(category) as string,
       selected: state.cardImagesCategory === category,
     }));
-  }
-
-  private static prepareDifficultyOptions(state: IGameSettingsState) {
-    return Object.entries(APP_GAME_DIFFICULTY_CONFIG).map((entry) => {
-      const cards = `${entry[1].cardField[0]} × ${entry[1].cardField[1]}`;
-      const time = String(entry[1].initialShowTime).padStart(2, '\xa0');
-      const score = Math.floor(100 * entry[1].scoreCoefficient);
-      return {
-        value: entry[0],
-        text: `🎴 ${cards} \xa0|\xa0 ⏱\xa0 ${time}s \xa0|\xa0 🏆 × ${score}%`,
-        selected: state.cardsAmount.toString() === entry[0],
-        title: `cards ${cards} \xa0|\xa0 start time ${time}s \xa0|\xa0 score × ${score}%`,
-      };
-    });
   }
 }
 
