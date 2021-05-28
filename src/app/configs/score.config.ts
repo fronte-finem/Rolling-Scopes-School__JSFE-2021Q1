9e5d69fc71b2616e4349308c576cde8d16f6aeb1
fronte-finem <fronte.finem+github@ya.ru>
2021-05-28 22:48:27 +0300

    refactor: cleaning score page component

---
+++ src/app/configs/score.config.ts
@@ -0,0 +1,8 @@
+// На странице Best score должен отображаться топ 10 игроков.
+export const SCORE_LIMIT = 10;
+
+export const enum ScoreTitle {
+  PLAYERS = 'Best players',
+  SCORE = 'Score',
+  TIME = 'Time',
+}
--- src/pages/score/score.ts
+++ src/pages/score/score.ts
@@ -1,22 +1,27 @@
+import { SCORE_LIMIT, ScoreTitle } from 'app/configs/score.config';
+import { ScoreRecordView } from 'components/score-record/score-record-view';
+
 import { IUser, IUserService, UserService } from '../../services/user-service';
-import { timeDiffFormat } from '../../shared/date-time-utils';
-import { renderAvatar } from '../../shared/views/avatar-factory';
 import { View } from '../../shared/views/view';
 import { BasePage } from '../base-page';
 
 import styles from './score.scss';
 
-const TITLE_PLAYERS = 'Best players';
-const TITLE_SCORE = 'Score';
-const TITLE_TIME = 'Time';
-// На странице Best score должен отображаться топ 10 игроков.
-const SCORE_LIMIT = 10;
+const createTitle = (text: string, classNames: string[]) =>
+  new View({ tag: 'h2', classNames, text });
 
 export class PageScore extends BasePage {
-  private readonly records = new View({
-    tag: 'ul',
-    classNames: [styles.users],
-  });
+  private title = new View({ classNames: [styles.title, styles.row] });
+
+  private titleCell1 = createTitle(ScoreTitle.PLAYERS, [styles.titleUsers]);
+
+  private titleCell2 = createTitle(ScoreTitle.SCORE, [styles.titleScore]);
+
+  private titleCell3 = createTitle(ScoreTitle.TIME, [styles.titleTime]);
+
+  private usersView = new View({ tag: 'ul', classNames: [styles.users] });
+
+  private recordsViews!: Array<ScoreRecordView>;
 
   public constructor(private readonly userService: IUserService) {
     super({ classNames: [styles.pageScore] });
@@ -28,83 +33,23 @@ export class PageScore extends BasePage {
 
   public async init(): Promise<void> {
     const users = await this.userService.getFirstByScore(SCORE_LIMIT);
+    this.title.render([this.titleCell1, this.titleCell2, this.titleCell3]);
+    this.view.render([this.title, this.usersView]);
+    this.initRows(users);
+  }
+
+  private initRows(users: IUser[]): void {
+    this.recordsViews = users.map(
+      (user) =>
+        new ScoreRecordView(user, { tag: 'li', classNames: [styles.row] })
+    );
     const { currentUser } = this.userService;
-    let checkUsers: (u: IUser) => boolean;
     if (currentUser) {
       const hash = UserService.userHashCode(currentUser);
-      checkUsers = (u) => hash === UserService.userHashCode(u);
-    } else {
-      checkUsers = () => false;
+      this.recordsViews
+        .filter((rec) => hash === UserService.userHashCode(rec.getUser()))?.[0]
+        .highlight();
     }
-    const items = users.map((user) => PageScore.createRecord(user, checkUsers));
-    this.view.render([PageScore.initTitle(), this.records.render(items)]);
-  }
-
-  private static initTitle(): View<HTMLElement> {
-    return new View({
-      classNames: [styles.title, styles.row],
-      childs: (
-        [
-          [TITLE_PLAYERS, [styles.cell1, styles.titleUsers]],
-          [TITLE_SCORE, [styles.cell2, styles.titleScore]],
-          [TITLE_TIME, [styles.cell3, styles.titleTime]],
-        ] as [string, string[]][]
-      ).map((opts) => PageScore.createTitle(...opts)),
-    });
-  }
-
-  private static createTitle(
-    text: string,
-    classNames: string[]
-  ): View<HTMLElement> {
-    return new View({
-      tag: 'h2',
-      classNames: [styles.titlePart, ...classNames],
-      text,
-    });
-  }
-
-  private static createRecord(
-    user: IUser,
-    checkUsers: (u: IUser) => boolean
-  ): View<HTMLLIElement> {
-    const itemClassNames = [styles.item, styles.row];
-    if (checkUsers(user)) itemClassNames.push(styles.itemCurrentUser);
-    const li = new View<HTMLLIElement>({
-      tag: 'li',
-      classNames: itemClassNames,
-    });
-
-    const avatar = new View({ classNames: [styles.avatar] });
-    renderAvatar(
-      user,
-      avatar,
-      [styles.svgIcon, styles.avatarImg],
-      [styles.avatarImg]
-    );
-
-    const name = new View({
-      classNames: [styles.userName],
-      text: `${user.firstName} ${user.lastName}`,
-    });
-    const mail = new View({
-      classNames: [styles.userMail],
-      text: `${user.email}`,
-    });
-    const full = new View({ classNames: [styles.cell1, styles.user] });
-
-    const score = new View({
-      classNames: [styles.cell2, styles.score],
-      text: `${user.score}`,
-    });
-
-    const time = timeDiffFormat(user.time);
-    const timeView = new View({
-      classNames: [styles.cell3, styles.time],
-      text: `${time.hours}:${time.min}:${time.sec}`,
-    });
-
-    li.render([avatar, full.render([name, mail]), score, timeView]);
-    return li;
+    this.usersView.render(this.recordsViews);
   }
 }
