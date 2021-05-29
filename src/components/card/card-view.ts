cb7e6c9be33e592f85cab4befaa0133da80683b7
fronte-finem <fronte.finem+github@ya.ru>
2021-05-29 23:50:34 +0300

    refactor: cleaning card-view

--- src/components/card/card-view.ts
+++ src/components/card/card-view.ts
@@ -6,24 +6,21 @@ import styles from './card-view.scss';
 
 const getImgUrl = (imgName: string) => `url("./images/${imgName}")`;
 
-const setBackgroundImg = (view: View, imgName: string) =>
-  view.setCssStyle('backgroundImage', getImgUrl(imgName));
-
 export class CardView extends View {
   public constructor(cardModel: CardModel) {
     super({ classNames: [styles.cardContainer] });
+    this.init(cardModel);
+  }
 
+  private init(cardModel: CardModel) {
     const back = new View({
       classNames: [styles.cardSide, styles.cardSideBack],
     });
-
     const front = new View({
       classNames: [styles.cardSide, styles.cardSideFront],
     });
-
-    setBackgroundImg(back, cardModel.backImage);
-    setBackgroundImg(front, cardModel.frontImage);
-
+    back.setCssStyle('backgroundImage', getImgUrl(cardModel.backImage));
+    front.setCssStyle('backgroundImage', getImgUrl(cardModel.frontImage));
     this.render(new View({ classNames: [styles.card], childs: [back, front] }));
   }
 
