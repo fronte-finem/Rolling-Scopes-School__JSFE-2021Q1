b8f2b1e8ff105254d602687b6ec5a35a674511cf
fronte-finem <fronte.finem+github@ya.ru>
2021-05-29 23:51:32 +0300

    feat: show more stats on score records

--- src/components/score-record/score-record-view.ts
+++ src/components/score-record/score-record-view.ts
@@ -1,46 +1,60 @@
 import { IUser } from 'services/user-service';
 import { timeDiffFormat } from 'shared/date-time-utils';
 import { renderAvatar } from 'shared/views/avatar-factory';
-import { ICreateViewOptions, View } from 'shared/views/view';
+import { View } from 'shared/views/view';
 
 import styles from './score-record-view.scss';
 
 export class ScoreRecordView extends View {
-  private avatar = new View({ classNames: [styles.avatar] });
-
-  private userView = new View({ classNames: [styles.user] });
-
-  private name = new View({ classNames: [styles.userName] });
-
-  private mail = new View({ classNames: [styles.userMail] });
-
-  private score = new View({ classNames: [styles.score] });
-
-  private time = new View({ classNames: [styles.time] });
-
-  public constructor(
-    private readonly user: IUser,
-    { classNames = [], ...options }: ICreateViewOptions
-  ) {
-    super({ classNames: [...classNames, styles.scoreRecord], ...options });
+  public constructor(private readonly user: IUser) {
+    super({ classNames: [styles.record] });
     this.init();
   }
 
   private init(): void {
+    const card = new View({ classNames: [styles.card] });
+    card.render([this.initFront(), this.initBack()]);
+    this.render(card);
+  }
+
+  private initFront(): View {
+    const front = new View({ classNames: [styles.side, styles.front] });
+    const avatar = new View({ classNames: [styles.avatar] });
+    const name = new View({ classNames: [styles.user, styles.name] });
+    const mail = new View({ classNames: [styles.user, styles.mail] });
+    const scoreView = new View({ classNames: [styles.score] });
+    this.initAvatar(avatar);
+    const { firstName, lastName, email, score } = this.user;
+    this.element.title = `${firstName}\n${lastName}`;
+    name.setText(`${firstName} ${lastName}`);
+    mail.setText(email);
+    scoreView.setText(String(score));
+    front.render([avatar, name, mail, scoreView]);
+    return front;
+  }
+
+  private initBack(): View {
+    const datetime = this.user.datetime?.toLocaleString() || 'unknown';
+    const cardsField = this.user.cardsField || 'unknown';
+    const { hours, min, sec } = timeDiffFormat(this.user.time || 0);
+    const back = new View(`<div class="${styles.side} ${styles.back}">
+      <div class="${styles.title}">Record date:</div>
+      <div class="${styles.datetime}">${datetime}</div>
+      <div class="${styles.title}">Cards field:</div>
+      <div class="${styles.cardsField}">${cardsField}</div>
+      <div class="${styles.title}">Game win time:</div>
+      <div class="${styles.time}">${hours}:${min}:${sec}</div>
+    </div>`);
+    return back;
+  }
+
+  private initAvatar(avatar: View): void {
     renderAvatar(
       this.user,
-      this.avatar,
+      avatar,
       [styles.svgIcon, styles.avatarImg],
       [styles.avatarImg]
     );
-    this.userView.render([this.name, this.mail]);
-    this.render([this.avatar, this.userView, this.score, this.time]);
-
-    this.name.setText(`${this.user.firstName} ${this.user.lastName}`);
-    this.mail.setText(this.user.email);
-    this.score.setText(String(this.user.score));
-    const timeDiff = timeDiffFormat(this.user.time);
-    this.time.setText(`${timeDiff.hours}:${timeDiff.min}:${timeDiff.sec}`);
   }
 
   public getUser(): IUser {
@@ -48,6 +62,6 @@ export class ScoreRecordView extends View {
   }
 
   public highlight(force = true): void {
-    this.setCssState(styles.scoreRecordHighlight, force);
+    this.setCssState(styles.highlight, force);
   }
 }
