56222d2d51271466be0a3717f6c96547e93800b2
fronte-finem <fronte.finem+github@ya.ru>
2021-05-30 19:14:02 +0300

    feat: add page-about ✔️

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,13 +1,46 @@
-import { View } from '../../shared/views/view';
+import { PAGE_ABOUT } from 'app/configs/page-about.config';
+import { zipWith } from 'shared/array-utils';
+import { ImgView } from 'shared/views/img/img';
+import { View } from 'shared/views/view';
+
 import { BasePage } from '../base-page';
+
+import styles from './about.scss';
+
+const createInfo = (text: string) => {
+  const info = new View({ classNames: [styles.info, styles.cell] });
+  const textView = new View({ classNames: [styles.text], text });
+  info.render(textView);
+  return info;
+};
+
+const createPict = (url: string) => {
+  const pict = new View({ classNames: [styles.pict, styles.cell] });
+  const img = new ImgView({ url, classNames: [styles.img] });
+  pict.render(img);
+  return pict;
+};
 
 export class PageAbout extends BasePage {
   public constructor() {
-    super({ classNames: ['page-about'] });
+    super({ classNames: [styles.pageAbout] });
   }
 
   public init(): void {
-    this.view.render(new View({ tag: 'h1', text: PageAbout.name }));
+    const title = new View({
+      tag: 'h2',
+      classNames: [styles.title],
+      text: PAGE_ABOUT.title,
+    });
+    const grid = new View({ classNames: [styles.grid] });
+    grid.render(
+      zipWith(
+        (text, url) => [createInfo(text), createPict(url)],
+        PAGE_ABOUT.texts,
+        PAGE_ABOUT.urls
+      ).flat()
+    );
+    this.view.render([title, grid]);
   }
 
   public stop(): void {
