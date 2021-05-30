bd774b84aef7f24bd81c5f1cc3a6c017a0bdbc9f
fronte-finem <fronte.finem+github@ya.ru>
2021-05-30 19:12:09 +0300

    feat: add content for page-about

---
+++ src/app/configs/page-about.config.ts
@@ -0,0 +1,15 @@
+export const PAGE_ABOUT = {
+  title: 'How to play?',
+
+  texts: [
+    'Register new player in game',
+    'Configure your game settings',
+    'Start you new game! Remember card positions and match it before times up.',
+  ],
+
+  urls: [
+    './images/screenshots/img1-signup.jpg',
+    './images/screenshots/img2-settings.jpg',
+    './images/screenshots/img3-game.jpg',
+  ],
+};
