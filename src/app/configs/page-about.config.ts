c1b306d674faff3aec842b4e7c7e7720a9ca3dec
fronte-finem <fronte.finem+github@ya.ru>
2021-05-31 15:27:36 +0300

    feat: add real view-steps instead screenshots for page-about

--- src/app/configs/page-about.config.ts
+++ src/app/configs/page-about.config.ts
@@ -1,15 +1,99 @@
+import { APP_CONFIG } from 'app/app.config';
+import { Card } from 'components/card/card';
+import { CardModel } from 'components/card/card-model';
+import { CardFieldModel } from 'components/cards-field/card-field-model';
+import { CardsField } from 'components/cards-field/cards-field';
+import { NavMenuView } from 'components/nav-menu/nav-menu-view';
+import { PopUpSignUpView } from 'components/pop-up-sign-up/pop-up-sign-up-view';
+import { DummyUserService } from 'services/dummy-user-service';
+import { randomFromInterval } from 'shared/numbers-utils';
+import { View } from 'shared/views/view';
+
+import { cardImagesService, gameSettingsService } from './services.config';
+
+import styles from '~pages/about/about.scss';
+
 export const PAGE_ABOUT = {
-  title: 'How to play?',
-
-  texts: [
-    'Register new player in game',
-    'Configure your game settings',
-    'Start you new game! Remember card positions and match it before times up.',
-  ],
-
-  urls: [
-    './images/screenshots/img1-signup.png',
-    './images/screenshots/img2-settings.png',
-    './images/screenshots/img3-game.png',
-  ],
+  title: {
+    tag: 'h2' as keyof HTMLElementTagNameMap,
+    text: 'How to play?',
+    classNames: [styles.title],
+  },
+  step1: {
+    text: 'Register new player in game',
+    userService: new DummyUserService(),
+  },
+  step2: { text: 'Configure your game settings' },
+  step3: {
+    cardFieldModel: new CardFieldModel(2, 3),
+    text: 'Start you new game! Remember card positions and match it before times up.',
+    gameSettingsService,
+    cardImagesService,
+  },
 };
+
+const createInfo = (text: string, className: string[] = []) => {
+  const info = new View({
+    classNames: [styles.info, styles.cell, ...className],
+  });
+  const textView = new View({ classNames: [styles.text], text });
+  info.render(textView);
+  return info;
+};
+
+const createPict = (content: View, className: string[] = []) => {
+  const pict = new View({
+    classNames: [styles.pict, styles.cell, ...className],
+  });
+  pict.render(content);
+  return pict;
+};
+
+export const createStep1 = (): View[] => {
+  const popup = new PopUpSignUpView(PAGE_ABOUT.step1.userService);
+  void popup.show();
+  return [
+    createInfo(PAGE_ABOUT.step1.text, [styles.infoStep1]),
+    createPict(popup, [styles.pictStep1]),
+  ];
+};
+
+export const createStep2 = (): View[] => {
+  const nav = new NavMenuView();
+  nav.init([APP_CONFIG.pages.settings]);
+  nav.setActiveNavLink(APP_CONFIG.pages.settings.route.url);
+  return [
+    createInfo(PAGE_ABOUT.step2.text, [styles.infoStep2]),
+    createPict(nav, [styles.pictStep2]),
+  ];
+};
+
+export const createStep3 = async (): Promise<View[]> => {
+  const cardsField = new CardsField();
+  const settings = PAGE_ABOUT.step3.gameSettingsService.load();
+  const urls = await PAGE_ABOUT.step3.cardImagesService.getUrls(
+    settings.cardImagesCategory,
+    PAGE_ABOUT.step3.cardFieldModel.getCardsAmount()
+  );
+  if (!urls) throw new Error('Cards images urls generation failed');
+  const cardModels = urls.front.map(
+    (url, id) => new CardModel(id, url, urls.back, settings.mismatchShowTime)
+  );
+  const cards = cardModels.map((model) => new Card(model));
+  matchRandom(cardModels);
+  cardsField.render(cards, PAGE_ABOUT.step3.cardFieldModel);
+  return [
+    createInfo(PAGE_ABOUT.step3.text, [styles.infoStep3]),
+    createPict(cardsField.view, [styles.pictStep3]),
+  ];
+};
+
+function matchRandom(cardModels: CardModel[]) {
+  const card = cardModels[randomFromInterval(0, cardModels.length - 1)];
+  cardModels
+    .filter((c) => c.frontImage === card.frontImage)
+    .forEach((c) => {
+      c.flip(true);
+      c.match(true);
+    });
+}
--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,45 +1,24 @@
-import { PAGE_ABOUT } from 'app/configs/page-about.config';
-import { zipWith } from 'shared/array-utils';
-import { ImgView } from 'shared/views/img/img';
+import {
+  createStep1,
+  createStep2,
+  createStep3,
+  PAGE_ABOUT,
+} from 'app/configs/page-about.config';
 import { View } from 'shared/views/view';
 
 import { BasePage } from '../base-page';
 
 import styles from './about.scss';
 
-const createInfo = (text: string) => {
-  const info = new View({ classNames: [styles.info, styles.cell] });
-  const textView = new View({ classNames: [styles.text], text });
-  info.render(textView);
-  return info;
-};
-
-const createPict = (url: string) => {
-  const pict = new View({ classNames: [styles.pict, styles.cell] });
-  const img = new ImgView({ url, classNames: [styles.img] });
-  pict.render(img);
-  return pict;
-};
-
 export class PageAbout extends BasePage {
   public constructor() {
     super({ classNames: [styles.pageAbout] });
   }
 
-  public init(): void {
-    const title = new View({
-      tag: 'h2',
-      classNames: [styles.title],
-      text: PAGE_ABOUT.title,
-    });
+  public async init(): Promise<void> {
+    const title = new View(PAGE_ABOUT.title);
     const grid = new View({ classNames: [styles.grid] });
-    grid.render(
-      zipWith(
-        (text, url) => [createInfo(text), createPict(url)],
-        PAGE_ABOUT.texts,
-        PAGE_ABOUT.urls
-      ).flat()
-    );
+    grid.render([createStep1(), createStep2(), await createStep3()].flat());
     this.view.render([title, grid]);
   }
 
