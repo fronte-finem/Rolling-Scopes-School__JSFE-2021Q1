8e4baa6e8747ee9ae8db4ee7f2f861239a38d76e
fronte-finem <fronte.finem+github@ya.ru>
2021-05-31 22:22:00 +0300

    fix: disable console.log

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,6 @@ import { App } from './app/app';
 window.addEventListener('load', () => {
   document.body.classList.add(styles.root);
   const app = new App(document.body);
-  app.start().catch((error: Error) => console.log(error));
+  // app.start().catch((error: Error) => console.log(error));
+  void app.start();
 });
