d2c9f1c190b24af852258d828382c63d64dfebab
fronte-finem <fronte.finem+github@ya.ru>
2021-06-08 00:02:36 +0300

    feat: add observers

---
+++ src/shared/observer.ts
@@ -0,0 +1,22 @@
+export type Listener<T> = (data: T) => unknown;
+
+type Listeners = Array<Listener<unknown>>;
+
+export class Observer<EventEnum> {
+  private readonly listenersMap = new Map<EventEnum, Listeners>();
+
+  public addListener<T>(event: EventEnum, listener: Listener<T>): void {
+    if (!this.listenersMap.has(event)) {
+      this.listenersMap.set(event, []);
+    }
+    this.listenersMap.get(event)?.push(listener as Listener<unknown>);
+  }
+
+  public removeListener<T>(event: EventEnum, listener: Listener<T>): void {
+    this.listenersMap.get(event)?.filter((x) => x !== listener);
+  }
+
+  public notify<T>(event: EventEnum, data: T): void {
+    this.listenersMap.get(event)?.forEach((listener) => listener(data));
+  }
+}
