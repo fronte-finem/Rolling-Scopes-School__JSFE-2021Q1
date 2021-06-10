9c783915d45702f4748fae46351f615a3bc04916
fronte-finem <fronte.finem+github@ya.ru>
2021-06-10 09:31:56 +0300

    refactor: extend car component

--- src/components/car/car-model.ts
+++ src/components/car/car-model.ts
@@ -1,48 +1,85 @@
 import { REST_API } from 'services/rest-api';
+import { Observer } from 'shared/observer';
+import { Maybe } from 'shared/types';
 
-export const CAR_EMPTY_ID = -1;
-export const CAR_EMPTY_NAME = '';
-export const CAR_EMPTY_COLOR = '#000000';
+import { CAR_EMPTY_COLOR, CAR_EMPTY_ID, CAR_EMPTY_NAME, CarEvent, CarState } from './car-congfig';
 
-export enum CarState {
-  INITIAL = 'initial',
-  DRIVE = 'race',
-  BROKEN = 'broken',
-}
-
-export class CarModel implements CarModel {
+export class CarModel {
+  private observer = new Observer<CarEvent>();
+  public id = CAR_EMPTY_ID;
+  public name = CAR_EMPTY_NAME;
+  public color = CAR_EMPTY_COLOR;
   public state = CarState.INITIAL;
   public position = 0;
-  private listener?: (model: CarModel) => void;
 
-  public constructor(
-    public id = CAR_EMPTY_ID,
-    public name = CAR_EMPTY_NAME,
-    public color = CAR_EMPTY_COLOR
-  ) {}
+  public duration: Maybe<number> = null;
 
-  public onUpdate(listener: (model: CarModel) => void): void {
-    this.listener = listener;
-  }
-
-  public move({ distance, velocity }: REST_API.IDriveParams): void {
-    this.state = CarState.DRIVE;
-    this.listener?.(this);
-    this.animateMove(Math.ceil(distance / velocity));
-  }
-
-  public pause(): void {
-    this.state = CarState.BROKEN;
-    this.listener?.(this);
+  public constructor(car?: REST_API.ICar) {
+    if (car) {
+      this.id = car.id;
+      this.name = car.name;
+      this.color = car.color;
+    }
   }
 
   public reset(): void {
-    this.state = CarState.INITIAL;
+    this.observer.reset();
+    this.id = CAR_EMPTY_ID;
+    this.name = CAR_EMPTY_NAME;
+    this.color = CAR_EMPTY_COLOR;
     this.position = 0;
-    this.listener?.(this);
   }
 
-  private animateMove(time: number): void {
+  public onUpdate(listener: (model: CarModel) => void): void {
+    this.observer.addListener(CarEvent.UPDATE, listener);
+  }
+
+  public update(name: string, color: string): CarModel {
+    this.name = name;
+    this.color = color;
+    this.observer.notify(CarEvent.UPDATE, this);
+    return this;
+  }
+
+  public onDrive(listener: (model: CarModel) => void): void {
+    this.observer.addListener(CarEvent.DRIVE, listener);
+  }
+
+  public driveStart(duration: Maybe<number>): CarModel {
+    this.state = CarState.START;
+    this.duration = duration;
+    this.observer.notify(CarEvent.DRIVE, this);
+    return this;
+  }
+
+  public drivePause(): CarModel {
+    this.state = CarState.BROKEN;
+    this.observer.notify(CarEvent.DRIVE, this);
+    return this;
+  }
+
+  public driveFinish(): CarModel {
+    this.state = CarState.FINISH;
+    this.observer.notify(CarEvent.DRIVE, this);
+    return this;
+  }
+
+  public driveReset(): CarModel {
+    this.state = CarState.INITIAL;
+    this.position = 0;
+    this.observer.notify(CarEvent.DRIVE, this);
+    return this;
+  }
+
+  public driveMove(): CarModel {
+    if (!this.duration) return this;
+    this.state = CarState.DRIVE;
+    this.observer.notify(CarEvent.DRIVE, this);
+    this.driveAnimate(this.duration);
+    return this;
+  }
+
+  private driveAnimate(time: number): void {
     let startTime = -1;
 
     const step = (timestamp: number): void => {
@@ -51,7 +88,7 @@ export class CarModel implements CarModel {
       const elapsed = timestamp - startTime;
       this.position = 100 * Math.min(elapsed / time, 1);
       if (elapsed < time) window.requestAnimationFrame(step);
-      this.listener?.(this);
+      this.observer.notify(CarEvent.DRIVE, this);
     };
 
     window.requestAnimationFrame(step);
--- src/components/car/car-view.ts
+++ src/components/car/car-view.ts
@@ -1,39 +1,44 @@
-import { createElement } from 'shared/dom-utils';
 import { View } from 'shared/view';
 
 import { CAR_CSS_CLASS, CAR_CSS_VAR } from './car.css';
-import { CarModel, CarState } from './car-model';
+import { CarState } from './car-congfig';
+import { CarModel } from './car-model';
 
 export class CarView extends View<CarModel> {
   private state = CarState.INITIAL;
-  private car = createElement(CAR_CSS_CLASS.body);
+
+  private stateMachine = new Map<CarState, (car: CarModel) => void>();
 
   public constructor(model: CarModel) {
-    super(CAR_CSS_CLASS.frame);
+    super(CAR_CSS_CLASS.car);
     this.init();
     model.onUpdate((car: CarModel) => this.update(car));
+    model.onDrive((car: CarModel) => this.drive(car));
     this.update(model);
   }
 
   protected init(): void {
-    this.root.append(this.car);
+    this.stateMachine.set(CarState.INITIAL, (car) => {
+      this.setCssState(CAR_CSS_CLASS.broken, false);
+      this.setCssState(CAR_CSS_CLASS.finish, false);
+      this.setCssVar(CAR_CSS_VAR.position, `${car.position}%`);
+    });
+    this.stateMachine.set(CarState.DRIVE, (car) => {
+      this.setCssVar(CAR_CSS_VAR.position, `${car.position}%`);
+    });
+    this.stateMachine.set(CarState.BROKEN, () => {
+      this.setCssState(CAR_CSS_CLASS.broken, true);
+    });
+    this.stateMachine.set(CarState.FINISH, () => {
+      this.setCssState(CAR_CSS_CLASS.finish, true);
+    });
   }
 
   public update(car: CarModel): void {
     this.setCssVar(CAR_CSS_VAR.color, car.color);
-    switch (car.state) {
-      case CarState.INITIAL:
-        this.setCssState(CAR_CSS_CLASS.broken, false);
-        this.setCssVar(CAR_CSS_VAR.position, `${car.position}%`);
-        break;
-      case CarState.DRIVE:
-        this.setCssVar(CAR_CSS_VAR.position, `${car.position}%`);
-        break;
-      case CarState.BROKEN:
-        this.setCssState(CAR_CSS_CLASS.broken, true);
-        break;
-      default:
-        break;
-    }
+  }
+
+  public drive(car: CarModel): void {
+    this.stateMachine.get(car.state)?.(car);
   }
 }
