f1147ce9376d5f6f8886d35510a6a07dedb725a9
fronte-finem <fronte.finem+github@ya.ru>
2021-06-10 09:45:11 +0300

    refactor: use controllers and services

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
 import { AppRoute, AppView } from 'app';
 import { GarageController } from 'pages/garage';
 import { WinnersController } from 'pages/winners';
-import { RaceService } from 'services/race';
+import { GarageService, RaceService, WinnersService } from 'services/race';
 
 import { root, theme } from './index.css';
 
 window.addEventListener('load', () => {
   document.body.classList.add(root, theme);
 
+  const garageService = new GarageService();
+  const winnersService = new WinnersService();
   const raceService = new RaceService();
 
-  const garageController = new GarageController(raceService);
-  const winnersController = new WinnersController(raceService);
+  const garageController = new GarageController(garageService, raceService);
+  const winnersController = new WinnersController(winnersService, raceService);
 
   const app = new AppView(AppRoute.GARAGE, garageController.view, winnersController.view);
   document.body.append(app.getRoot());
