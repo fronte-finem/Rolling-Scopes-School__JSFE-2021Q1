f4cee4bcba015cf00f1278a5bc0f25b28d7b6f88
fronte-finem <fronte.finem+github@ya.ru>
2021-06-12 00:41:39 +0300

    feat: add winners-header component

---
+++ src/components/winners-header/winners-header-config.ts
@@ -0,0 +1,13 @@
+export enum WinnersHeaderName {
+  NUM = 'number',
+  CAR = 'car',
+  NAME = 'name',
+  WINS = 'wins',
+  TIME = 'best time (seconds)',
+}
+
+export enum WinnersHeaderState {
+  INITIAL = 'initial',
+  SORT_WINS = 'sort wins',
+  SORT_TIME = 'sort time',
+}
