7e8acf08855a6f335a4a45e8dfed5baac4a07d7b
fronte-finem <fronte.finem+github@ya.ru>
2021-06-16 19:49:50 +0300

    feat: add sorting requests for winners-page

--- src/pages/winners/controller.ts
+++ src/pages/winners/controller.ts
@@ -1,17 +1,15 @@
 import { CarsService, RaceService } from 'services/race';
 
-import { WinnersModel } from './winners-model';
-import { WinnersView } from './winners-view';
+import { WinnersModel } from './model';
+import { WinnersView } from './view';
 
 export class WinnersController {
-  public readonly view: WinnersView;
-
   public constructor(
     public readonly model: WinnersModel,
+    public readonly view: WinnersView,
     private readonly carsService: CarsService,
     private readonly raceService: RaceService
   ) {
-    this.view = new WinnersView(model);
     this.initBinds();
   }
 
@@ -26,5 +24,12 @@ export class WinnersController {
 
     this.raceService.onRaceWin((car) => this.carsService.addWinner(car));
     this.carsService.onAddWinner = (car) => this.view.add(car);
+
+    this.view.onRequestPage((num) =>
+      this.carsService.getWinnersPage(num, this.view.sortType, this.view.sortOrder)
+    );
+
+    this.view.onRequesSortWins = (order) => this.carsService.sortWins(order);
+    this.view.onRequesSortTime = (order) => this.carsService.sortTime(order);
   }
 }
--- src/pages/winners/winners.css.ts
+++
@@ -1,7 +0,0 @@
-import { style } from '@vanilla-extract/css';
-
-const winners = style({});
-
-export const WINNERS_CSS_CLASS = {
-  winners,
-};
