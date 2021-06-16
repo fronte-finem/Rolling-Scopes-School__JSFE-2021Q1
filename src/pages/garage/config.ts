b9c281209e121097e2a5c38383a8fdecaa47137e
fronte-finem <fronte.finem+github@ya.ru>
2021-06-16 23:21:58 +0300

    refactor: remove unneeded  buttons

--- src/pages/garage/config.ts
+++ src/pages/garage/config.ts
@@ -3,10 +3,9 @@ export const GENERATE_COUNT = 100;
 export const GARAGE = 'Garage';
 
 export enum CarageButtons {
-  ADD = 'add',
-  UPDATE = 'update',
-  UNSELECT = 'unselect',
-  GENERATE = 'generate',
+  CREATE = 'Create <BUG>',
+  UPDATE = 'Update <BUG>',
+  GENERATE = 'generate 100 <BUG>',
   REMOVE_PAGE = 'remove page',
   RACE = 'race',
   RESET = 'reset',
