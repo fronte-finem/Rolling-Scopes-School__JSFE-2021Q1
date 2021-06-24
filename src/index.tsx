8574986d50abffad9c3ab2120bcf52e0ebfd3fb9
fronte-finem <fronte.finem+github@ya.ru>
2021-06-24 18:20:59 +0300

    feat: fetch data with use hooks

--- src/index.tsx
+++ src/index.tsx
@@ -1,64 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
-import { Card } from 'card/card';
-import { CardsField } from 'cards-field/cards-field';
-
-const cardsData = [
-  {
-    word: 'cry',
-    translation: 'плакать',
-    image: 'data/img/cry.jpg',
-    audio: 'data/audio/cry.mp3',
-  },
-  {
-    word: 'dance',
-    translation: 'танцевать',
-    image: 'data/img/dance.jpg',
-    audio: 'data/audio/dance.mp3',
-  },
-  {
-    word: 'dive',
-    translation: 'нырять',
-    image: 'data/img/dive.jpg',
-    audio: 'data/audio/dive.mp3',
-  },
-  {
-    word: 'draw',
-    translation: 'рисовать',
-    image: 'data/img/draw.jpg',
-    audio: 'data/audio/draw.mp3',
-  },
-  {
-    word: 'fish',
-    translation: 'ловить рыбу',
-    image: 'data/img/fish.jpg',
-    audio: 'data/audio/fish.mp3',
-  },
-  {
-    word: 'fly',
-    translation: 'летать',
-    image: 'data/img/fly.jpg',
-    audio: 'data/audio/fly.mp3',
-  },
-  {
-    word: 'hug',
-    translation: 'обнимать',
-    image: 'data/img/hug.jpg',
-    audio: 'data/audio/hug.mp3',
-  },
-  {
-    word: 'jump',
-    translation: 'прыгать',
-    image: 'data/img/jump.jpg',
-    audio: 'data/audio/jump.mp3',
-  },
-];
+import { CardsField } from 'components/cards-field/cards-field';
+import { CardDTO, CategoryDTO } from 'types/dto';
+import { fetchData } from 'services/fetch';
 
 const App = () => {
+  const [data, setData] = useState([] as Array<CardDTO>);
+
+  useEffect(() => {
+    (async () => {
+      const data = await fetchData();
+      if (data.length === 0) return;
+      setData(data[2].cards);
+    })();
+    return;
+  }, []);
+
   return (
     <div>
       <h1>English for kids!</h1>
-      <CardsField cards={cardsData} />
+      <CardsField cards={data} />
     </div>
   );
 };
