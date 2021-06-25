fd982c8be111c9ceecd1c8236af48cc6741b46b4
fronte-finem <fronte.finem+github@ya.ru>
2021-06-25 21:28:44 +0300

    refactor: extract button-menu styles

--- src/components/btn-menu/btn-menu.tsx
+++ src/components/btn-menu/btn-menu.tsx
@@ -1,49 +1,25 @@
 import React from 'react';
-import styled from 'styled-components';
+import { StyledProps } from 'types/styled';
+import {
+  StyledBtnMenu,
+  StripeTopLeft,
+  StripeTopRight,
+  StripeMiddle,
+  StripeBottomLeft,
+  StripeBottomRight,
+  StyledWrapper,
+} from './style';
 
-export const StyledBtnMenu = styled.button`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: stretch;
-  width: 60px;
-  height: 60px;
-  padding: 0;
-  border: none;
-  background: transparent;
-
-  &:hover {
-    --middle: scaleX(0);
-    --top: rotate(45deg) scaleX(1.414);
-    --bottom: rotate(-45deg) scaleX(1.414);
-  }
-`;
-
-export const Stripe = styled.span`
-  display: block;
-  height: 10px;
-  background: #333;
-  border-radius: 5px;
-  transform: var(--middle);
-  transform-origin: center left;
-  transition: all 500ms;
-`;
-export const StripeOuter = styled(Stripe)`
-  transform-origin: 5px;
-`;
-export const StripeTop = styled(StripeOuter)`
-  transform: var(--top);
-`;
-export const StripeBottom = styled(StripeOuter)`
-  transform: var(--bottom);
-`;
-
-export const BtnMenu = () => {
+export const BtnMenu = ({ className }: StyledProps) => {
   return (
-    <StyledBtnMenu>
-      <StripeTop />
-      <Stripe />
-      <StripeBottom />
+    <StyledBtnMenu className={className}>
+      <StyledWrapper>
+        <StripeTopLeft />
+        <StripeTopRight />
+        <StripeMiddle />
+        <StripeBottomLeft />
+        <StripeBottomRight />
+      </StyledWrapper>
     </StyledBtnMenu>
   );
 };
---
+++ src/components/btn-menu/style.tsx
@@ -0,0 +1,87 @@
+import styled, { css } from 'styled-components';
+
+export const StyledBtnMenu = styled.button`
+  --scale-x: scaleX(1.5);
+  --stripe-s: 5px;
+  --stripe-w: calc(50% + var(--stripe-s));
+  --stripe-h: calc(2 * var(--stripe-s));
+  position: relative;
+  width: 50px;
+  height: 50px;
+  padding: 0;
+  border: none;
+  background: transparent;
+
+  &:hover {
+    --middle: scaleX(0);
+    --clockwise: rotate(45deg) scaleX(1.4142);
+    --counter-clockwise: rotate(-45deg) scaleX(1.4142);
+    --scale-x: scaleX(1);
+  }
+`;
+
+export const StyledWrapper = styled.span`
+  position: absolute;
+  top: 0;
+  left: -50%;
+  width: 100%;
+  height: 100%;
+  transition: all 200ms ease-in-out;
+  transform: translateX(50%) var(--scale-x);
+`;
+
+const Stripe = styled.span`
+  display: block;
+  position: absolute;
+  width: var(--stripe-w);
+  height: var(--stripe-h);
+  background: #333;
+  border-radius: var(--stripe-s);
+  transition: all 200ms ease-in-out;
+`;
+
+export const StripeMiddle = styled(Stripe)`
+  top: calc(50% - var(--stripe-s));
+  width: 100%;
+  transform-origin: center;
+  transform: var(--middle);
+`;
+
+const left = css`
+  left: 0;
+  transform-origin: var(--stripe-s) var(--stripe-s);
+`;
+const right = css`
+  right: 0;
+  transform-origin: calc(100% - var(--stripe-s)) var(--stripe-s);
+`;
+const top = css`
+  top: 0;
+`;
+const bottom = css`
+  bottom: 0;
+`;
+
+export const StripeTopLeft = styled(Stripe)`
+  ${top}
+  ${left}
+  transform: var(--clockwise);
+`;
+
+export const StripeTopRight = styled(Stripe)`
+  ${top}
+  ${right}
+  transform: var(--counter-clockwise);
+`;
+
+export const StripeBottomLeft = styled(Stripe)`
+  ${bottom}
+  ${left}
+  transform: var(--counter-clockwise);
+`;
+
+export const StripeBottomRight = styled(Stripe)`
+  ${bottom}
+  ${right}
+  transform: var(--clockwise);
+`;
