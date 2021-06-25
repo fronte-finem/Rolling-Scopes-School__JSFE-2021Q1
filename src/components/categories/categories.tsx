f4fb09b6ba4be7092a5ac2803d2aa58a2e272f67
fronte-finem <fronte.finem+github@ya.ru>
2021-06-25 16:34:59 +0300

    feat: add category-routes

---
+++ src/components/categories/categories.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { HashRouter, Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { CategoryDTO } from 'types/category-dto';
+import { CategoryCard } from '../category/category';
+
+const StyledCategories = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  grid-gap: 20px;
+`;
+
+const CARD_WIDTH = 300;
+
+const StyledCategoriesItem = styled.li`
+  display: block;
+  flex: 0 0 ${CARD_WIDTH}px;
+  perspective: ${CARD_WIDTH * 3}px;
+`;
+
+const StyledLink = styled(Link)`
+  display: block;
+`;
+
+export interface CategoriesProps {
+  data: CategoryDTO[];
+}
+
+export const Categories = ({ data }: CategoriesProps) => {
+  return (
+    <nav>
+      <StyledCategories>
+        {data.map((categoryDTO, id) => (
+          <StyledCategoriesItem key={categoryDTO.category}>
+            <StyledLink to={`/${categoryDTO.path}`} draggable={false}>
+              <CategoryCard data={categoryDTO} />
+            </StyledLink>
+          </StyledCategoriesItem>
+        ))}
+      </StyledCategories>
+    </nav>
+  );
+};
