f75ef1fa800ea3a1463f985125c1cf3de6678c52
fronte-finem <fronte.finem+github@ya.ru>
2021-06-27 20:51:26 +0300

    refactor: update DTO interfaces and validators

--- src/types/category-dto.ts
+++ src/types/category-dto.ts
@@ -1,11 +1,10 @@
 import { Validator } from './abstract';
-import { CardDTO, cardDTOValidator } from './card-dto';
 
 export interface CategoryDTO {
+  readonly id: number;
   readonly category: string;
   readonly path: string;
   readonly image: string;
-  readonly cards: Array<CardDTO>;
 }
 
 type CategoryDTOValidator = Validator<CategoryDTO>;
@@ -13,11 +12,10 @@ type CategoryDTOValidator = Validator<CategoryDTO>;
 export const categoryDTOValidator: CategoryDTOValidator = (data: unknown) => {
   if (!(data instanceof Object)) return null;
   const maybeCategoryDTO = data as Record<string, unknown>;
+  if (typeof maybeCategoryDTO.id !== 'number') return null;
   if (typeof maybeCategoryDTO.category !== 'string') return null;
   if (typeof maybeCategoryDTO.path !== 'string') return null;
   if (typeof maybeCategoryDTO.image !== 'string') return null;
-  if (!Array.isArray(maybeCategoryDTO.cards)) return null;
-  if (maybeCategoryDTO.cards.some((item) => cardDTOValidator(item) === null)) return null;
   return maybeCategoryDTO as unknown as CategoryDTO;
 };
 
---
+++ src/types/word-dto.ts
@@ -0,0 +1,32 @@
+import { Validator } from './abstract';
+
+export interface WordDTO {
+  readonly id: number;
+  readonly categoryId: number;
+  readonly word: string;
+  readonly translation: string;
+  readonly image: string;
+  readonly audio: string;
+}
+
+type WordDTOValidator = Validator<WordDTO>;
+
+export const wordDTOValidator: WordDTOValidator = (data: unknown) => {
+  if (!(data instanceof Object)) return null;
+  const maybeWordDTO = data as Record<string, unknown>;
+  if (typeof maybeWordDTO.id !== 'number') return null;
+  if (typeof maybeWordDTO.categoryId !== 'number') return null;
+  if (typeof maybeWordDTO.word !== 'string') return null;
+  if (typeof maybeWordDTO.translation !== 'string') return null;
+  if (typeof maybeWordDTO.image !== 'string') return null;
+  if (typeof maybeWordDTO.audio !== 'string') return null;
+  return maybeWordDTO as unknown as WordDTO;
+};
+
+type WordsDTOValidator = Validator<WordDTO[]>;
+
+export const wordsDTOValidator: WordsDTOValidator = (data: unknown) => {
+  if (!Array.isArray(data)) return null;
+  if (data.some((item) => wordDTOValidator(item) === null)) return null;
+  return data as WordDTO[];
+};
