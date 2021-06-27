0f984a64ca374158935b02833759ffe6f433d8ed
fronte-finem <fronte.finem+github@ya.ru>
2021-06-27 20:52:02 +0300

    refactor: update fix-links service

--- src/services/fix-links.ts
+++ src/services/fix-links.ts
@@ -1,18 +1,18 @@
-import { CardDTO } from 'types/card-dto';
+import { Maybe } from 'types/abstract';
 import { CategoryDTO } from 'types/category-dto';
+import { WordDTO } from 'types/word-dto';
 
-export function fixLinks(data: CategoryDTO[]): CategoryDTO[] {
-  return data.map(({ image, cards, ...rest }) => ({
+export function fixCategoriesLinks(data: Maybe<CategoryDTO[]>): CategoryDTO[] {
+  return (data || []).map(({ image, ...rest }) => ({
     ...rest,
     image: `data/${image}`,
-    cards: fixCardLinks(cards),
   }));
 }
 
-function fixCardLinks(cards: CardDTO[]): CardDTO[] {
-  return cards.map(({ image, audio, ...rest }) => ({
+export function fixWordsLinks(words: Maybe<WordDTO[]>): WordDTO[] {
+  return (words || []).map(({ image, audio, ...rest }) => ({
+    ...rest,
     image: `data/${image}`,
     audio: `data/${audio}`,
-    ...rest,
   }));
 }
