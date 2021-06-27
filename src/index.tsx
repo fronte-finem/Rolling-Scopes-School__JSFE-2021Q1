83fa09234ea800118cd76288ea571ea8e30f8a89
fronte-finem <fronte.finem+github@ya.ru>
2021-06-27 20:54:16 +0300

    refactor: update data fetch

--- src/index.tsx
+++ src/index.tsx
@@ -3,46 +3,61 @@ import ReactDOM from 'react-dom';
 import { HashRouter, Route, Switch, useParams } from 'react-router-dom';
 
 import { CardsField } from 'components/cards-field/cards-field';
-import { Categories, CategoriesProps } from 'components/categories/categories';
+import { Categories } from 'components/categories/categories';
 import { Header } from 'components/header/header';
 import { Sidebar } from 'components/sidebar/sidebar';
 import { initialSidebarState, SidebarContext, sidebarReducer } from 'contexts/sidebar-context';
 import { useFetch } from 'services/fetch-hook';
-import { fixLinks } from 'services/fix-links';
-import { categoriesDTOValidator } from 'types/category-dto';
+import { fixCategoriesLinks, fixWordsLinks } from 'services/fix-links';
+import { categoriesDTOValidator, CategoryDTO } from 'types/category-dto';
+import { WordDTO, wordsDTOValidator } from 'types/word-dto';
 
-const Cards = ({ data }: CategoriesProps) => {
+import { Maybe } from './types/abstract';
+
+interface CardsProps {
+  words: Maybe<WordDTO[]>;
+  categories: Maybe<CategoryDTO[]>;
+}
+
+const Cards = ({ words, categories }: CardsProps) => {
   const { slug } = useParams<{ slug: string }>();
-  const cards = data.find((dto) => dto.path === slug)?.cards;
-  return cards ? <CardsField cards={cards} /> : <>404</>;
+  const category = fixCategoriesLinks(categories).find((dto) => dto.path === slug);
+  const wordsDTO = fixWordsLinks(words).filter((dto) => dto.categoryId === category?.id);
+  return wordsDTO.length > 0 ? <CardsField words={wordsDTO} /> : <>404</>;
 };
 
 const App = () => {
-  const [data, isLoading, error] = useFetch('./data/cards.json', categoriesDTOValidator);
+  const [categories, isCategoriesLoading, categoriesError] = useFetch(
+    './data/categories.json',
+    categoriesDTOValidator
+  );
+  const [words, isWordsLoading, wordsError] = useFetch('./data/words.json', wordsDTOValidator);
   const [sidebarState, closeSidebar] = React.useReducer(sidebarReducer, initialSidebarState);
 
-  if (isLoading) return <>Loading...</>;
-  if (error) return <>Error...</>;
+  if (isCategoriesLoading) return <>Loading Categories...</>;
+  if (isWordsLoading) return <>Loading Words...</>;
+  if (categoriesError) return <>Categories Load Error: ${categoriesError.message}</>;
+  if (wordsError) return <>Words Load Error: ${wordsError.message}</>;
 
   return (
     <div>
       <SidebarContext.Provider value={{ sidebarState, closeSidebar }}>
         <Header />
         <HashRouter>
-          <div>
-            <Sidebar data={data === null ? [] : fixLinks(data)} />
+          <main>
+            <Sidebar categories={fixCategoriesLinks(categories)} />
             <Switch>
               <Route exact path="/">
-                <Categories data={data === null ? [] : fixLinks(data)} />
+                <Categories categories={fixCategoriesLinks(categories)} />
               </Route>
               <Route path="/about">
                 <>ABOUT</>
               </Route>
               <Route path="/:slug">
-                <Cards data={data === null ? [] : fixLinks(data)} />
+                <Cards words={words} categories={categories} />
               </Route>
             </Switch>
-          </div>
+          </main>
         </HashRouter>
       </SidebarContext.Provider>
     </div>
