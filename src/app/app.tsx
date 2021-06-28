289bb04f17eb4352a6e8a8f1226c3c18be529a30
fronte-finem <fronte.finem+github@ya.ru>
2021-06-28 21:08:35 +0300

    refactor: add game context provider

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -7,32 +7,37 @@ import { Footer } from 'components/footer/footer';
 import { Header } from 'components/header/header';
 import { Sidebar } from 'components/sidebar/sidebar';
 import { DataContext, fetchData } from 'services/data-context';
+import { GameContext, useGameCycle } from 'services/game';
 
 import { Main, StyledApp } from './style';
 
 export const App = (): JSX.Element => {
+  const [gameState, dispatch] = useGameCycle();
+
   return (
     <DataContext.Provider value={fetchData()}>
-      <StyledApp>
-        <Header />
-        <Main>
-          <HashRouter>
-            <Sidebar />
-            <Switch>
-              <Route exact path="/">
-                <Categories />
-              </Route>
-              <Route path="/about">
-                <>ABOUT</>
-              </Route>
-              <Route path="/:categoryPath">
-                <CardsField />
-              </Route>
-            </Switch>
-          </HashRouter>
-        </Main>
-        <Footer />
-      </StyledApp>
+      <GameContext.Provider value={{ gameState, dispatch }}>
+        <StyledApp>
+          <Header />
+          <Main>
+            <HashRouter>
+              <Sidebar />
+              <Switch>
+                <Route exact path="/">
+                  <Categories />
+                </Route>
+                <Route path="/about">
+                  <>ABOUT</>
+                </Route>
+                <Route path="/:categoryPath">
+                  <CardsField />
+                </Route>
+              </Switch>
+            </HashRouter>
+          </Main>
+          <Footer />
+        </StyledApp>
+      </GameContext.Provider>
     </DataContext.Provider>
   );
 };
