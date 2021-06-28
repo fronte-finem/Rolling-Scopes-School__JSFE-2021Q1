507e88375e26c94af35204d62685384b07117748
fronte-finem <fronte.finem+github@ya.ru>
2021-06-28 15:23:26 +0300

    refactor: use data context

--- src/components/cards-field/cards-field.tsx
+++ src/components/cards-field/cards-field.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
+import { useWordsData } from 'services/data-context';
 import { StyledProps } from 'types/styled';
 import { WordDTO } from 'types/word-dto';
 
 import { StyledCardsField, StyledCardsFieldItem } from './style';
 
-export interface CardsFieldProps extends StyledProps {
-  words: WordDTO[];
-}
+export const CardsField = ({ className }: StyledProps): JSX.Element => {
+  const { categoryPath } = useParams<{ categoryPath: string }>();
+  const words: string | WordDTO[] = useWordsData(categoryPath);
+  if (typeof words === 'string') return <h2>{words}</h2>;
 
-export const CardsField = ({ words, className }: CardsFieldProps): JSX.Element => {
   return (
     <StyledCardsField className={className}>
-      {words.map((wordDTO) => (
-        <StyledCardsFieldItem key={wordDTO.word} wordDTO={wordDTO} />
+      {words.map((dto) => (
+        <StyledCardsFieldItem key={dto.word} wordDTO={dto} />
       ))}
     </StyledCardsField>
   );
--- src/components/categories/categories.tsx
+++ src/components/categories/categories.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
 import { CategoryLink } from 'components/category/category';
+import { useCategoriesData } from 'services/data-context';
 import { CategoryDTO } from 'types/category-dto';
 import { StyledProps } from 'types/styled';
 
 import { StyledCategories, StyledCategoriesItem } from './style';
 
-export interface CategoriesProps extends StyledProps {
-  categories: CategoryDTO[];
-}
+export const Categories = ({ className }: StyledProps): JSX.Element => {
+  const categories: string | CategoryDTO[] = useCategoriesData();
+  if (typeof categories === 'string') return <h2>{categories}</h2>;
 
-export const Categories = ({ className, categories }: CategoriesProps): JSX.Element => {
   return (
     <nav className={className}>
       <StyledCategories>
