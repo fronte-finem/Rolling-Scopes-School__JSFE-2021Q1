cc9cc758c041a133b7c00018685d3358b1fbac1d
fronte-finem <fronte.finem+github@ya.ru>
2021-06-29 23:38:46 +0300

    refactor: remove use of game context from categories view

--- src/components/categories/categories.tsx
+++ src/components/categories/categories.tsx
@@ -1,25 +1,14 @@
-import React, { useContext, useEffect } from 'react';
+import React from 'react';
 
 import { CategoryLink } from 'components/category/category';
 import { useCategoriesData } from 'services/data-context';
-import { GameActionType, GameContext, GameDispatch, GameState, GameStatus } from 'services/game';
 import { CategoryDTO } from 'types/category-dto';
 import { StyledProps } from 'types/styled';
 
 import { StyledCategories, StyledCategoriesItem } from './style';
 
-function useGameEnd(gameState: GameState, dispatch: GameDispatch) {
-  useEffect(() => {
-    if (gameState.status !== GameStatus.INITIAL) {
-      dispatch({ type: GameActionType.END });
-    }
-  }, []);
-}
-
 export const Categories = ({ className }: StyledProps): JSX.Element => {
   const categories: string | CategoryDTO[] = useCategoriesData();
-  const { gameState, dispatch } = useContext(GameContext);
-  useGameEnd(gameState, dispatch);
   if (typeof categories === 'string') return <h2>{categories}</h2>;
 
   return (
