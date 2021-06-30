caae655a1ad588146dcc96e4317572b80d54c2ec
fronte-finem <fronte.finem+github@ya.ru>
2021-06-30 16:41:45 +0300

    feat: tune routing for game-end states

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,17 +1,32 @@
-import React, { useEffect } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { FC, useEffect } from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 import { CardsField } from 'components/cards-field/cards-field';
 import { Categories } from 'components/categories/categories';
 import { useCategoryLocation } from 'components/category/category-link';
 import { Footer } from 'components/footer/footer';
 import { Header } from 'components/header/header';
+import { FailPage, WinPage } from 'components/page-game-end/page-game-end';
 import { Sidebar } from 'components/sidebar/sidebar';
 import { GameActionType } from 'services/game/game-action';
 import { useGameContext } from 'services/game/game-context';
-import { isGameStarted, isOtherCategory } from 'services/game/game-state';
+import {
+  GameState,
+  isEnd,
+  isFail,
+  isGameStarted,
+  isOtherCategory,
+  isWin,
+} from 'services/game/game-state';
 
 import { Main, StyledApp } from './app-style';
+
+const GameRoute: FC<{ state: GameState }> = ({ state }) => {
+  if (isEnd(state)) return <Redirect to="/" />;
+  if (isWin(state)) return <WinPage />;
+  if (isFail(state)) return <FailPage />;
+  return <CardsField />;
+};
 
 export const App = (): JSX.Element => {
   const category = useCategoryLocation();
@@ -19,7 +34,7 @@ export const App = (): JSX.Element => {
 
   useEffect(() => {
     if (isGameStarted(gameState) && isOtherCategory(gameState, category)) {
-      dispatch({ type: GameActionType.END, payload: null });
+      dispatch({ type: GameActionType.RESET });
     }
   }, [category]);
 
@@ -35,8 +50,14 @@ export const App = (): JSX.Element => {
           <Route path="/about">
             <>ABOUT</>
           </Route>
+          <Route path="/win">
+            <WinPage />
+          </Route>
+          <Route path="/fail">
+            <FailPage />
+          </Route>
           <Route path="/category/:category">
-            <CardsField />
+            <GameRoute state={gameState} />
           </Route>
           <Route path="*">
             <h2>Error 404: Page Not found</h2>
