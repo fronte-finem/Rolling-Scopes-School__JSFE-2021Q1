83c7955849516a8b3d0a4e3b3268c23d38a2f65a
fronte-finem <fronte.finem+github@ya.ru>
2021-07-02 22:18:26 +0300

    feat: add test-page for components

--- src/app/test-route.tsx
+++ src/app/test-route.tsx
@@ -2,11 +2,13 @@ import React, { FC } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { FailPage, WinPage } from 'components/page-game-end/page-game-end';
+import { TestPage } from 'components/test-page/test-page';
 
 const TestRoutesMap = new Map<string, JSX.Element>([
   ['about', <h2>ABOUT</h2>],
   ['win', <WinPage />],
   ['fail', <FailPage />],
+  ['test', <TestPage />],
 ]);
 
 const NoTestRoute: FC<{ component: string }> = ({ component }) => {
---
+++ src/components/test-page/test-page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { BtnStartRepeat } from 'components/header/btn-start-repeat';
+
+export const TestPage: React.FC = () => {
+  return (
+    <div>
+      <BtnStartRepeat onClick={() => {}} isStart />
+      <BtnStartRepeat onClick={() => {}} />
+    </div>
+  );
+};
