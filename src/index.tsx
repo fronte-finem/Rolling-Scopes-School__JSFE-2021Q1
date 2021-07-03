07ce504a4a4ea33b70c93abdb6289bb23bba57f0
fronte-finem <fronte.finem+github@ya.ru>
2021-07-03 16:25:48 +0300

    feat: add words-statistic context provider to app root

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,18 @@ import { HashRouter } from 'react-router-dom';
 import { App } from 'app/app';
 import { DataContextProvider } from 'services/data/data-context';
 import { GameContextProvider } from 'services/game/game-context';
+import { WordsStatsContextProvider } from 'services/stats/words-stats-context';
 
 ReactDOM.render(
   <React.StrictMode>
     <DataContextProvider>
-      <GameContextProvider>
-        <HashRouter>
-          <App />
-        </HashRouter>
-      </GameContextProvider>
+      <WordsStatsContextProvider>
+        <GameContextProvider>
+          <HashRouter>
+            <App />
+          </HashRouter>
+        </GameContextProvider>
+      </WordsStatsContextProvider>
     </DataContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
