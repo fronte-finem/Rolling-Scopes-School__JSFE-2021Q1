afb1b8a67270bb4b1d751b200f3e8f975491ead2
fronte-finem <fronte.finem+github@ya.ru>
2021-07-03 16:28:50 +0300

    feat: integrate words statistic updaters

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -3,6 +3,7 @@ import React, { useRef, useState } from 'react';
 import { Marks } from 'components/card/marks';
 import { playAudio } from 'services/audio';
 import { WordDTO } from 'services/data/dto-word';
+import { useWordsStatsContext } from 'services/stats/words-stats-context';
 import { StyledProps } from 'types/styled';
 import { randomItem } from 'utils/random';
 
@@ -30,26 +31,33 @@ const SAD = ['very-sad', 'confused', 'arrogant', 'sad', 'bored'];
 const CARD_MARKS_LIMIT = -6;
 
 export const Card = (props: CardProps): JSX.Element => {
-  const { className, wordDTO, isGameMode, isSolved, matchWord } = props;
-  const { word, translation, image, audio } = wordDTO;
+  const { className, wordDTO, isGameMode, isGameReady, isGamePlay, isSolved, matchWord } = props;
+  const { id, word, translation, image, audio } = wordDTO;
   const ref = useRef<HTMLButtonElement>(null);
   const [isFlipped, setFlip] = useState(false);
   const [marks, setMark] = useState<string[]>([]);
+  const { askClick, flipClick, gameClick, matchClick } = useWordsStatsContext();
   const cardClassName = getCardClassName(isFlipped, props);
 
   const handlePlay = (ev: React.MouseEvent<HTMLDivElement>) => {
-    if (isGameMode) {
-      if (isSolved) return;
-      const emo = randomItem(matchWord(wordDTO) ? HAPPY : SAD);
+    if (isGameReady || isSolved) return;
+    if (isGamePlay) {
+      const isMatch = matchWord(wordDTO);
+      const emo = randomItem(isMatch ? HAPPY : SAD);
       setMark([...marks.slice(CARD_MARKS_LIMIT), emo]);
+      isMatch ? matchClick(id) : gameClick(id);
       return;
     }
     if (ref.current?.contains(ev.target as Node)) return;
     playAudio(audio);
+    askClick(id);
   };
 
   const handleMouseLeave = () => setFlip(() => false);
-  const handleFlip = () => setFlip(() => true);
+  const handleFlip = () => {
+    setFlip(() => true);
+    flipClick(id);
+  };
 
   return (
     <CardContainer className={className} onMouseLeave={handleMouseLeave}>
