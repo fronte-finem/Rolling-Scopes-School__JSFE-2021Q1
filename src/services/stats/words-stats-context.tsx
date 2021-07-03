5838d1359ca127fba955f4200a5306b48ab0c734
fronte-finem <fronte.finem+github@ya.ru>
2021-07-03 16:21:57 +0300

    feat: add words-statistic state management

---
+++ src/services/stats/words-stats-context.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+
+import { useDataContext } from 'services/data/data-context';
+import { FetchStatus } from 'services/data/fetch-hook';
+import { useLocalStorage } from 'utils/local-storage-hook';
+
+import {
+  addAskClick,
+  addFlipClick,
+  addGameClick,
+  addMatchClick,
+  getInitialWordStats,
+  WordStats,
+} from './word-stats';
+
+type WordsStatsUpdater = (id: number, words: WordStats[]) => WordStats[];
+type WordsStatsPersist = (id: number) => void;
+
+const factoryPersistUpdater = (
+  words: WordStats[],
+  updater: WordsStatsUpdater,
+  persist: React.Dispatch<React.SetStateAction<WordStats[]>>
+): WordsStatsPersist => {
+  return (id: number): void => {
+    const updatedWords = updater(id, words);
+    persist(updatedWords);
+  };
+};
+
+interface IWordsStatsContext {
+  wordsStats: WordStats[];
+  askClick: WordsStatsPersist;
+  flipClick: WordsStatsPersist;
+  gameClick: WordsStatsPersist;
+  matchClick: WordsStatsPersist;
+  resetStats: () => void;
+}
+
+const WordsStatsContext = React.createContext<IWordsStatsContext | undefined>(undefined);
+
+const STORAGE_KEY = 'fronte-finem--efk--words-stats';
+
+export const WordsStatsContextProvider: React.FC = ({ children }) => {
+  const { wordsState } = useDataContext();
+
+  const wordsData = wordsState.status !== FetchStatus.SUCCESS ? [] : wordsState.data;
+  const initialWordsStats = wordsData ? wordsData.map((dto) => getInitialWordStats(dto.id)) : [];
+
+  const [wordsStats, setWordsStats] = useLocalStorage<WordStats[]>(STORAGE_KEY, initialWordsStats);
+
+  if (wordsStats.length === 0 && initialWordsStats.length > 0) {
+    setWordsStats(initialWordsStats);
+  }
+
+  const initialValue: IWordsStatsContext = {
+    wordsStats,
+    askClick: factoryPersistUpdater(wordsStats, addAskClick, setWordsStats),
+    flipClick: factoryPersistUpdater(wordsStats, addFlipClick, setWordsStats),
+    gameClick: factoryPersistUpdater(wordsStats, addGameClick, setWordsStats),
+    matchClick: factoryPersistUpdater(wordsStats, addMatchClick, setWordsStats),
+    resetStats: () => setWordsStats(wordsStats.map(([id]) => getInitialWordStats(id))),
+  };
+
+  return <WordsStatsContext.Provider value={initialValue}>{children}</WordsStatsContext.Provider>;
+};
+
+export const useWordsStatsContext = (): IWordsStatsContext => {
+  const context = React.useContext(WordsStatsContext);
+
+  if (context === undefined) {
+    throw Error('WordsStatsContext must be used inside of a WordsStatsContextProvider');
+  }
+
+  return context;
+};
