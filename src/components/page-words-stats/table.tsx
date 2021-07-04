1b3bca1cc9016debbeb3733d60f1c4ef51f03aa9
fronte-finem <fronte.finem+github@ya.ru>
2021-07-04 21:06:37 +0300

    feat: implement sorting for table columns

--- src/components/page-words-stats/table.tsx
+++ src/components/page-words-stats/table.tsx
@@ -1,36 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { useDataContextWithChecks } from 'services/data/data-context';
 import { useWordsStatsContext } from 'services/stats/words-stats-context';
 
-import { TableHeader } from './table-header';
-import { TableRow } from './table-row';
+import { OrderField, TableHeader } from './table-header';
+import { getRowProps, Row, RowProps } from './table-row';
 import { StyledTable, Tbody } from './table-style';
+import { WordDTO } from 'services/data/dto-word';
+import { CategoryDTO } from 'services/data/dto-category';
+import { WordStats } from 'services/stats/word-stats';
+import { Order } from 'components/page-words-stats/head-cell';
+
+const getRowsProps = (words: WordDTO[], categories: CategoryDTO[], wordsStats: WordStats[]) => {
+  return words
+    .map((wordDTO): null | RowProps => {
+      const category = categories.find(({ id }) => id === wordDTO.categoryId)?.category;
+      if (!category) return null;
+      const stats = wordsStats.find(([id]) => id === wordDTO.id);
+      if (!stats) return null;
+      return getRowProps(category, wordDTO, stats);
+    })
+    .filter((props) => props !== null) as RowProps[];
+};
+
+type Comparator = (a: RowProps, b: RowProps) => number;
+
+function compare(order: Order, key: OrderField): Comparator {
+  return function (a: RowProps, b: RowProps) {
+    if (order === Order.NONE) return 0;
+    const [propA, propB] = [a.data[key], b.data[key]];
+    if (typeof propA === 'string' && typeof propB === 'string')
+      return order === Order.ASC ? propA.localeCompare(propB) : propB.localeCompare(propA);
+    if (typeof propA === 'number' && typeof propB === 'number')
+      return order === Order.ASC ? propA - propB : propB - propA;
+    return 0;
+  };
+}
+
+const sortRowsProps = (props: RowProps[], field: OrderField, order: Order) => {
+  if (order === Order.NONE) return [...props].sort((a, b) => a.id - b.id);
+  return [...props].sort(compare(order, field));
+};
 
 export const Table: React.FC = () => {
   const [categoriesData, wordsData] = useDataContextWithChecks();
   const { wordsStats } = useWordsStatsContext();
+  const [rowsProps, setRowsProps] = useState(getRowsProps(wordsData, categoriesData, wordsStats));
+
+  useEffect(() => {
+    setRowsProps(getRowsProps(wordsData, categoriesData, wordsStats));
+  }, [wordsData, categoriesData, wordsStats]);
+
+  const handleOrderChange = (field: OrderField, order: Order) => {
+    setRowsProps(sortRowsProps(rowsProps, field, order));
+  };
 
   return (
     <StyledTable>
-      <TableHeader />
+      <TableHeader onOrderChange={handleOrderChange} />
       <Tbody>
-        {wordsData.map((wordDTO, index): null | JSX.Element => {
-          const category = categoriesData.find(({ id }) => id === wordDTO.categoryId)?.category;
-          if (!category) return null;
-          const stats = wordsStats.find(([id]) => id === wordDTO.id);
-          if (!stats) return null;
-          return (
-            <TableRow
-              key={wordDTO.id}
-              index={index + 1}
-              category={category}
-              word={wordDTO.word}
-              translation={wordDTO.translation}
-              stats={stats}
-            />
-          );
-        })}
+        {rowsProps.map(({ id, data }, index) => (
+          <Row key={id} id={id} index={index + 1} data={data} />
+        ))}
       </Tbody>
     </StyledTable>
   );
