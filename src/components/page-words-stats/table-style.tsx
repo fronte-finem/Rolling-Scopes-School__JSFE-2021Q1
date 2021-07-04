38b0699a627bd5bcb5a335327cd4a2b81d156723
fronte-finem <fronte.finem+github@ya.ru>
2021-07-04 22:45:34 +0300

    feat: make table scrollable

--- src/components/page-words-stats/table-style.tsx
+++ src/components/page-words-stats/table-style.tsx
@@ -1,34 +1,25 @@
 import styled from 'styled-components';
 
 export const StyledTable = styled.table`
+  position: relative;
   width: 100%;
-  border-radius: 15px;
+  min-width: 1200px;
   box-shadow: 0 0 20px #0004, 0 10px 20px #0002, 0 20px 20px #0002, 0 30px 20px #0002;
 `;
 
 export const Thead = styled.thead`
-  border-radius: inherit;
-
-  tr:first-child {
-    border-radius: inherit;
-    *:first-child {
-      border-top-left-radius: inherit;
-    }
-    *:last-child {
-      border-top-right-radius: inherit;
-    }
-  }
+  position: sticky;
+  top: 0;
+  z-index: 2;
 `;
 
 export const Tbody = styled.tbody`
-  border-radius: inherit;
-
   tr {
     transition: all 100ms;
   }
 
   &:hover tr {
-    opacity: 0.5;
+    opacity: 0.8;
   }
 
   &:hover tr:hover {
@@ -38,15 +29,5 @@ export const Tbody = styled.tbody`
 
   tr:nth-child(2n) {
     background: #0002;
-  }
-
-  tr:last-child {
-    border-radius: inherit;
-    td:first-child {
-      border-bottom-left-radius: inherit;
-    }
-    td:last-child {
-      border-bottom-right-radius: inherit;
-    }
   }
 `;
