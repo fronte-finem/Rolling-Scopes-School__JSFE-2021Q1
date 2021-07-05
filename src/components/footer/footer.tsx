04e579b0a8d0ddd40bf2c6c5be293aeb0c358aca
fronte-finem <fronte.finem+github@ya.ru>
2021-07-05 23:25:05 +0300

    feat: add footer content

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,13 +2,42 @@ import React from 'react';
 
 import { StyledProps } from 'types/styled';
 
-import { StyledFooter, StyledHeading, Wrapper } from './footer-style';
+import { Group, LogoLink, LogoSvg, LogoText, StyledFooter, Wrapper } from './footer-style';
+
+const GITHUB_LOGO = './svg/sprite.svg#icon-github';
+const RS_SCHOOL_LOGO = './svg/sprite.svg#icon-rs-school-js';
+const YEAR = "'21";
+const MENTOR = 'dimonwhite';
+const STUDENT = 'fronte-finem';
 
 export const Footer = ({ className }: StyledProps): JSX.Element => {
   return (
     <StyledFooter className={className}>
       <Wrapper>
-        <StyledHeading>F O O T E R</StyledHeading>
+        <LogoLink className="rss" href="https://rs.school/js/">
+          <LogoSvg className="rss">
+            <use href={RS_SCHOOL_LOGO} />
+          </LogoSvg>
+          <LogoText className="rss">{YEAR}</LogoText>
+        </LogoLink>
+        <Group>
+          <LogoLink className="github" href="https://github.com/dimonwhite">
+            <LogoSvg className="github">
+              <use href={GITHUB_LOGO} />
+            </LogoSvg>
+            <LogoText className="github">
+              mentor<span className="spoiler">: {MENTOR}</span>
+            </LogoText>
+          </LogoLink>
+          <LogoLink className="github" href="https://github.com/fronte-finem">
+            <LogoSvg className="github">
+              <use href={GITHUB_LOGO} />
+            </LogoSvg>
+            <LogoText className="github">
+              student<span className="spoiler">: {STUDENT}</span>
+            </LogoText>
+          </LogoLink>
+        </Group>
       </Wrapper>
     </StyledFooter>
   );
