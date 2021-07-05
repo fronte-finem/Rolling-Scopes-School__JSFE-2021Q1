bdadfb6d577ae6fa924ba7b10c2995841c0a70ad
fronte-finem <fronte.finem+github@ya.ru>
2021-07-05 23:24:16 +0300

    refactor: add route for difficult words page

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, useLocation } from 'react-router-dom';
 
-import { useCategoryLocation } from 'components/category/category-link';
 import { Footer } from 'components/footer/footer';
 import { Header } from 'components/header/header';
 import { PageCategories } from 'components/page-categories/page-categories';
@@ -9,7 +8,7 @@ import { PageWordsStats } from 'components/page-words-stats/page-words-stats';
 import { Sidebar } from 'components/sidebar/sidebar';
 import { GameActionType } from 'services/game/game-action';
 import { useGameContext } from 'services/game/game-context';
-import { isGameStarted, isOtherCategory } from 'services/game/game-state';
+import { isGameStarted, isOtherRoutePath } from 'services/game/game-state';
 
 import { Main, StyledApp } from './app-style';
 import { GameRoute } from './game-route';
@@ -17,14 +16,14 @@ import { NotFoundRoute } from './not-found-route';
 import { TestRoute } from './test-route';
 
 export const App = (): JSX.Element => {
-  const category = useCategoryLocation();
+  const { pathname } = useLocation();
   const { gameState, dispatch } = useGameContext();
 
   useEffect(() => {
-    if (isGameStarted(gameState) && isOtherCategory(gameState, category)) {
+    if (isGameStarted(gameState) && isOtherRoutePath(gameState, pathname)) {
       dispatch({ type: GameActionType.RESET });
     }
-  }, [category]);
+  }, [pathname]);
 
   return (
     <StyledApp>
@@ -37,6 +36,9 @@ export const App = (): JSX.Element => {
           </Route>
           <Route path="/category/:category">
             <GameRoute state={gameState} />
+          </Route>
+          <Route path="/difficult">
+            <GameRoute state={gameState} isDifficultWords />
           </Route>
           <Route path="/statistic">
             <PageWordsStats />
