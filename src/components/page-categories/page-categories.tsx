cab0f184b0c9d354ac66485e06c8b90f2178ea24
fronte-finem <fronte.finem+github@ya.ru>
2021-07-15 09:52:41 +0300

    refactor: update categories page

--- src/components/page-categories/page-categories.tsx
+++ src/components/page-categories/page-categories.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import { Main } from 'app/app-style';
 import { CategoryLink } from 'components/category/category';
-import { useCategoriesData } from 'services/data/data-context';
-import { CategoryDTO } from 'services/data/dto-category';
+import { Header } from 'components/header/header';
+import { Sidebar } from 'components/sidebar/sidebar';
+import { useDataContext } from 'services/data/data-context';
 import { useGameContext } from 'services/game/game-context';
 import { isGameMode } from 'services/game/game-state';
 import { StyledProps } from 'types/styled';
@@ -10,19 +12,24 @@ import { StyledProps } from 'types/styled';
 import { StyledCategories, StyledCategoriesItem } from './page-categories-style';
 
 export const PageCategories = ({ className }: StyledProps): JSX.Element => {
-  const categories: string | CategoryDTO[] = useCategoriesData();
+  const { categoriesData } = useDataContext();
   const { gameState } = useGameContext();
-  if (typeof categories === 'string') return <h2>{categories}</h2>;
 
   return (
-    <nav className={className}>
-      <StyledCategories>
-        {categories.map((categoryDTO) => (
-          <StyledCategoriesItem key={categoryDTO.category}>
-            <CategoryLink categoryDTO={categoryDTO} isGameMode={isGameMode(gameState)} />
-          </StyledCategoriesItem>
-        ))}
-      </StyledCategories>
-    </nav>
+    <>
+      <Sidebar />
+      <Header />
+      <Main>
+        <nav className={className}>
+          <StyledCategories>
+            {categoriesData.map((data) => (
+              <StyledCategoriesItem key={data.category._id}>
+                <CategoryLink data={data} isGameMode={isGameMode(gameState)} />
+              </StyledCategoriesItem>
+            ))}
+          </StyledCategories>
+        </nav>
+      </Main>
+    </>
   );
 };
