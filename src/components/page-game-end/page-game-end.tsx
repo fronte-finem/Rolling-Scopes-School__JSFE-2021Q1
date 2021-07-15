1fe82f1b2bfa551bd8757b61ab081adc54342fd1
fronte-finem <fronte.finem+github@ya.ru>
2021-07-15 09:52:21 +0300

    refactor: update game-end page

--- src/components/page-game-end/page-game-end.tsx
+++ src/components/page-game-end/page-game-end.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
+import { Main } from 'app/app-style';
 import { EMOJI_NEGATIVE, EMOJI_POSITIVE } from 'components/emoji/emoji';
+import { Header } from 'components/header/header';
+import { Sidebar } from 'components/sidebar/sidebar';
 import { useGameContext } from 'services/game/game-context';
 import { StyledProps } from 'types/styled';
 
@@ -13,20 +16,32 @@ export const GameEndPage: React.FC<StyledProps> = ({ className, children }) => {
 
 export const WinPage: React.FC<StyledProps> = ({ className }) => {
   return (
-    <GameEndPage className={className}>
-      <EmojiList emojiNames={EMOJI_POSITIVE} />
-    </GameEndPage>
+    <>
+      <Sidebar />
+      <Header />
+      <Main>
+        <GameEndPage className={className}>
+          <EmojiList emojiNames={EMOJI_POSITIVE} />
+        </GameEndPage>
+      </Main>
+    </>
   );
 };
 
 export const FailPage: React.FC<StyledProps> = ({ className }) => {
   const { gameState } = useGameContext();
   return (
-    <GameEndPage className={className}>
-      <FailWrapper>
-        <FailMessage>{gameState.mistakes} mistakes</FailMessage>
-        <EmojiList emojiNames={EMOJI_NEGATIVE} />
-      </FailWrapper>
-    </GameEndPage>
+    <>
+      <Sidebar />
+      <Header />
+      <Main>
+        <GameEndPage className={className}>
+          <FailWrapper>
+            <FailMessage>{gameState.mistakes} mistakes</FailMessage>
+            <EmojiList emojiNames={EMOJI_NEGATIVE} />
+          </FailWrapper>
+        </GameEndPage>
+      </Main>
+    </>
   );
 };
