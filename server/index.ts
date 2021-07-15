fec4d02761cd90a05c48657589ec42249d53d6f5
fronte-finem <fronte.finem+github@ya.ru>
2021-07-15 09:29:53 +0300

    feat: add entry point for server app

---
+++ server/index.ts
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+import { app } from './app';
+
+dotenv.config();
+
+const APP_PORT = Number(process.env.APP_PORT) || 5000;
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/test';
+console.log(APP_PORT, MONGO_URI);
+if (!MONGO_URI) throw new Error('Env variable MONGO_URI unset');
+
+const mongodbOptions: mongoose.ConnectOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose
+  .connect(MONGO_URI, mongodbOptions)
+  .then(() => console.log('Connected to mongo database'))
+  .catch((error) => console.log(error));
+
+const logStart = (port: number) => () => console.log(`Server started on http://localhost:${port}`);
+
+app.listen(APP_PORT, logStart(APP_PORT));
