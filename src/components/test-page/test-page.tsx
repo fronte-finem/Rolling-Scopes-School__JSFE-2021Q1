702178f82a5ec2af8ca6cb38fd38986d8e78df35
fronte-finem <fronte.finem+github@ya.ru>
2021-07-15 09:48:37 +0300

    refactor: update test page

--- src/components/test-page/test-page.tsx
+++ src/components/test-page/test-page.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
+import { Main } from 'app/app-style';
 import { BtnStartRepeat } from 'components/header/btn-start-repeat';
+import { Header } from 'components/header/header';
+import { Sidebar } from 'components/sidebar/sidebar';
 
 export const TestPage: React.FC = () => {
   return (
-    <div>
-      <BtnStartRepeat onClick={() => {}} isStart />
-      <BtnStartRepeat onClick={() => {}} />
-    </div>
+    <>
+      <Sidebar />
+      <Header />
+      <Main>
+        <div>
+          <BtnStartRepeat onClick={() => {}} isStart />
+          <BtnStartRepeat onClick={() => {}} />
+        </div>
+      </Main>
+    </>
   );
 };
