8ca73f02b9fe8889067e1afd2d727070c2f4cacd
fronte-finem <fronte.finem+github@ya.ru>
2021-07-15 09:54:04 +0300

    refactor: simplify header view

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
 
 import { ModeSwitch } from 'components/header/mode-switch';
-import { getWords } from 'services/data/data-context';
-import { GameActionType } from 'services/game/game-action';
 import { useGameContext } from 'services/game/game-context';
-import { isGameMode, isGameReady, isGameStarted } from 'services/game/game-state';
-import { useWordsStatsContext } from 'services/stats/words-stats-context';
+import { isGameStarted } from 'services/game/game-state';
 import { StyledProps } from 'types/styled';
 
 import { BtnStartRepeat } from './btn-start-repeat';
@@ -19,60 +15,40 @@ import {
   Wrapper,
 } from './header-style';
 
-const isCategoryPath = (routePath: string) => routePath.startsWith('/category');
-const isDifficultWordsPath = (routePath: string) => routePath.startsWith('/difficult');
+interface Props extends StyledProps {
+  isAdmin?: boolean;
+  showBtnStartRepeat?: boolean;
+  onStartRepeat?: () => void;
+}
 
-const getCategoryName = (routePath: string): string => {
-  return isCategoryPath(routePath) ? routePath.replace(/^\/category\//, '') : '';
-};
-
-export const Header = ({ className }: StyledProps): JSX.Element => {
-  const { wordsData, categoriesData, getDifficultWords } = useWordsStatsContext();
-  const { pathname } = useLocation();
-  const { gameState, dispatch } = useGameContext();
-  const [hideBtnStart, setHideBtnStart] = React.useState(true);
-
-  const isCategory = isCategoryPath(pathname);
-  const isDifficultWords = isDifficultWordsPath(pathname);
-  const categoryName = getCategoryName(pathname);
-
-  React.useEffect(() => {
-    if (isDifficultWords && getDifficultWords().length === 0) {
-      setHideBtnStart(true);
-      return;
-    }
-    setHideBtnStart(!(isCategory || isDifficultWords) || !isGameMode(gameState));
-  }, [isCategory, isDifficultWords, gameState]);
-
-  const handleChangeMode = (isSecond: boolean) => {
-    dispatch({ type: isSecond ? GameActionType.DISABLE : GameActionType.ENABLE });
-  };
-
-  const handleStartGame = () => {
-    if (isCategory || isDifficultWords) {
-      if (isGameReady(gameState)) {
-        const words = isDifficultWords
-          ? getDifficultWords()
-          : getWords(categoryName, categoriesData, wordsData);
-        dispatch({ type: GameActionType.START, payload: { routePath: pathname, words } });
-      } else {
-        dispatch({ type: GameActionType.VOCALIZE });
-      }
-    }
-  };
+export const Header: React.FC<Props> = ({
+  children,
+  className,
+  isAdmin = false,
+  showBtnStartRepeat = false,
+  onStartRepeat = () => {},
+}) => {
+  const { gameState } = useGameContext();
 
   return (
     <StyledHeader className={className}>
       <Wrapper>
-        <StyledHeading>
-          <StyledLink to="/">English for kids</StyledLink>
-        </StyledHeading>
-        <BtnStartRepeatWrapper isHidden={hideBtnStart}>
-          <BtnStartRepeat onClick={handleStartGame} isStart={!isGameStarted(gameState)} />
-        </BtnStartRepeatWrapper>
-        <ModeSwitchWrapper>
-          <ModeSwitch firstName="train" secondName="play" changeMode={handleChangeMode} />
-        </ModeSwitchWrapper>
+        {isAdmin ? (
+          children
+        ) : (
+          <>
+            <StyledHeading>
+              <StyledLink to="/">English for kids</StyledLink>
+            </StyledHeading>
+            {children}
+            <BtnStartRepeatWrapper isHidden={!showBtnStartRepeat}>
+              <BtnStartRepeat onClick={onStartRepeat} isStart={!isGameStarted(gameState)} />
+            </BtnStartRepeatWrapper>
+            <ModeSwitchWrapper>
+              <ModeSwitch />
+            </ModeSwitchWrapper>
+          </>
+        )}
       </Wrapper>
     </StyledHeader>
   );
--- src/components/header/mode-switch.tsx
+++ src/components/header/mode-switch.tsx
@@ -1,34 +1,25 @@
 import React from 'react';
 
-import { StyledProps } from 'types/styled';
+import { GameActionType } from 'services/game/game-action';
+import { useGameContext } from 'services/game/game-context';
+import { isGameMode } from 'services/game/game-state';
 
 import { ModeFirst, ModeSecond, StyledModeSwitch, Switch } from './mode-switch-style';
 
-interface Props extends StyledProps {
-  firstName: string;
-  secondName: string;
-  changeMode: (isSecond: boolean) => void;
-}
+export const ModeSwitch = (): JSX.Element => {
+  const { gameState, dispatch } = useGameContext();
+  const isGame = isGameMode(gameState);
 
-export const ModeSwitch = ({
-  className,
-  firstName,
-  secondName,
-  changeMode,
-}: Props): JSX.Element => {
-  const [isSecond, setSecond] = React.useState(false);
-
-  const switchClassName = `${className || ''} ${isSecond ? 'second' : ''}`;
+  const switchClassName = isGame ? 'second' : '';
 
   const handleChangeMode = () => {
-    setSecond(!isSecond);
-    changeMode(isSecond);
+    dispatch({ type: isGame ? GameActionType.DISABLE : GameActionType.ENABLE });
   };
 
   return (
     <StyledModeSwitch className={switchClassName} onClick={handleChangeMode}>
-      <ModeFirst>{firstName}</ModeFirst>
-      <ModeSecond>{secondName}</ModeSecond>
+      <ModeFirst>train</ModeFirst>
+      <ModeSecond>play</ModeSecond>
       <Switch />
     </StyledModeSwitch>
   );
