334adae1df6a463a8b3207f7f3d3cba331fd59fc
fronte-finem <fronte.finem+github@ya.ru>
2021-07-17 15:49:34 +0300

    refactor: add updateItemById function

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,6 +1,18 @@
-export function updateArray<T>(array: T[], item: T, comparator: (x: T) => (y: T) => boolean): T[] {
+
+export function updateItem<T>(array: T[], item: T, comparator: (x: T) => (y: T) => boolean): T[] {
   const predicate = comparator(item);
   const index = array.findIndex(predicate);
   if (index < 0) return array;
   return [...array.slice(0, index), item, ...array.slice(index + 1)];
 }
+
+interface WithId {
+  id: string;
+}
+
+export function updateItemById<T extends WithId>(id: string, array: T[], update: (x: T) => T): T[] {
+  const index = array.findIndex((value) => value.id === id);
+  if (index < 0) return array;
+  return [...array.slice(0, index), update(array[index]), ...array.slice(index + 1)];
+}
+
