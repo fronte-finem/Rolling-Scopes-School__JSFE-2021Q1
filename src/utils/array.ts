27bd6c82aae64c65bf7acd02f3b7d7e9cb12c543
fronte-finem <fronte.finem+github@ya.ru>
2021-07-17 15:50:41 +0300

    feat: add abstract sort array by order and key wrapper

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,3 +1,4 @@
+import { Order } from 'types/order';
 
 export function updateItem<T>(array: T[], item: T, comparator: (x: T) => (y: T) => boolean): T[] {
   const predicate = comparator(item);
@@ -16,3 +17,25 @@ export function updateItemById<T extends WithId>(id: string, array: T[], update:
   return [...array.slice(0, index), update(array[index]), ...array.slice(index + 1)];
 }
 
+function applyOrder<T>(order: Order, a: T, b: T) {
+  if (order === Order.NONE) return 0;
+  if (typeof a === 'string' && typeof b === 'string')
+    return order === Order.ASC ? a.localeCompare(b) : b.localeCompare(a);
+  if (typeof a === 'number' && typeof b === 'number') return order === Order.ASC ? a - b : b - a;
+  return 0;
+}
+
+export function sort<T, K extends keyof T>(order: Order, array: T[], key: K): T[] {
+  if (order === Order.NONE) return array;
+  return [...array].sort((a: T, b: T) => applyOrder(order, a[key], b[key]));
+}
+
+export function sort2<T, K1 extends keyof T, K2 extends keyof T[K1]>(
+  order: Order,
+  array: T[],
+  key1: K1,
+  key2: K2
+): T[] {
+  if (order === Order.NONE) return array;
+  return [...array].sort((a: T, b: T) => applyOrder(order, a[key1][key2], b[key1][key2]));
+}
