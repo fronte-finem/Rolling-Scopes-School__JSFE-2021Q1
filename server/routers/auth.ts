71a7b6839e63e9cf98c84732a1124a58391b4988
fronte-finem <fronte.finem+github@ya.ru>
2021-07-19 16:16:40 +0300

    refactor: remove logout route

--- server/routers/auth.ts
+++ server/routers/auth.ts
@@ -1,13 +1,11 @@
 import { Router } from 'express';
 
-import { login, logout, tokenVerified } from '../controllers/auth';
+import { login, tokenVerified } from '../controllers/auth';
 import { verifyToken } from '../middlewares/auth';
 
 const authRouter = Router();
 
 authRouter.post('/login', login);
-
-authRouter.get('/logout', logout);
 
 authRouter.get('/check-token', [verifyToken], tokenVerified);
 
