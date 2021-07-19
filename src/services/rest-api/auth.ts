19aa0698af40a18215da345529b40180dd188876
fronte-finem <fronte.finem+github@ya.ru>
2021-07-19 16:25:40 +0300

    fix: change client auth-service login handler

--- src/services/rest-api/auth.ts
+++ src/services/rest-api/auth.ts
@@ -1,29 +1,40 @@
-import { User } from '@server/stores/auth';
-import axios from 'axios';
+import { LoginProps } from '@server/models/user';
+import axios, { AxiosError } from 'axios';
 
-import { CURRENT_API_URL } from 'services/rest-api/config';
-
-type UserRes = User & {
-  accessToken?: string;
-};
+import { BACKEND_API_URL } from 'services/rest-api/config';
 
 export const AUTH_STORAGE_KEY = 'fronte-finem--efk--auth-token';
 
-const API_URL = `${CURRENT_API_URL}/api/auth`;
+const API_URL = `${BACKEND_API_URL}/api/auth`;
+
+export interface RestApiResponse<T> {
+  data?: T;
+  error?: string;
+}
+
+async function axiosWrapper<T>(axiosPromise: () => Promise<T>): Promise<RestApiResponse<T>> {
+  try {
+    return { data: await axiosPromise() };
+  } catch (error) {
+    console.log('AxiosError Error:', (error as AxiosError).response);
+    return {
+      error: (error as AxiosError).response?.data as string,
+    };
+  }
+}
 
 class AuthService {
-  public login = async (user: User): Promise<UserRes> => {
-    const response = await axios.post<UserRes>(`${API_URL}/login`, user);
-
-    if (response.data.accessToken) {
-      localStorage.setItem(AUTH_STORAGE_KEY, response.data.accessToken);
-    }
-
-    return response.data;
+  public login = async (loginProps: LoginProps): Promise<RestApiResponse<string>> => {
+    return axiosWrapper(async () => {
+      const { data } = await axios.post<string>(`${API_URL}/login`, loginProps);
+      if (data) {
+        localStorage.setItem(AUTH_STORAGE_KEY, data);
+      }
+      return data;
+    });
   };
 
-  public logout = async (): Promise<void> => {
-    await axios.get(`${API_URL}/logout`, { headers: authHeader() });
+  public logout = (): void => {
     localStorage.removeItem(AUTH_STORAGE_KEY);
   };
 
