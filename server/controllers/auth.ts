9d07dcfbec233629982240b66d43837be09f4f6e
fronte-finem <fronte.finem+github@ya.ru>
2021-07-19 16:21:52 +0300

    refactor: remove logout-controller and simplify login-controller

--- server/controllers/auth.ts
+++ server/controllers/auth.ts
@@ -1,38 +1,21 @@
-import dotenv from 'dotenv';
 import { Request, Response } from 'express';
-import { sign } from 'jsonwebtoken';
 
-import { accessTokens, admin, isAdmin, User } from '../stores/auth';
+import { getAdmin, LoginProps } from '../models/user';
+import { createAccessToken } from '../services/auth';
+import { getErrorMsg } from '../utils/error';
 
-dotenv.config();
-
-const JWT_SECRET = process.env.JWT_SECRET || 'not secret';
-const JWT_EXPIRES_IN = 86400; // 24 hours
-
-export function login(request: Request, response: Response): Response {
-  if (!isAdmin(request.body as User)) {
-    return response.status(401).send('login failed');
+export async function login(request: Request, response: Response): Promise<Response> {
+  try {
+    const admin = await getAdmin();
+    if (!admin.verify(request.body as LoginProps)) {
+      return response.status(401).send('Login failed!');
+    }
+    return response.status(200).send(createAccessToken(admin));
+  } catch (error) {
+    return response.status(500).send(getErrorMsg(error));
   }
-
-  const accessToken = sign({ username: admin.username }, JWT_SECRET, {
-    expiresIn: JWT_EXPIRES_IN,
-  });
-
-  accessTokens.add(accessToken);
-
-  return response.status(200).json({
-    username: admin.username,
-    password: '',
-    accessToken,
-  } as User);
-}
-
-export function logout(request: Request, response: Response): void {
-  const token = request.headers['x-access-token'] as string;
-  accessTokens.delete(token);
-  response.status(200).send('logout success!');
 }
 
 export function tokenVerified(request: Request, response: Response): void {
-  response.status(200).json({ msg: 'Token valid!' });
+  response.sendStatus(200);
 }
