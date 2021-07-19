551c639b16af9292b05981f5b837d11f36b9ae42
fronte-finem <fronte.finem+github@ya.ru>
2021-07-19 23:46:00 +0300

    refactor: reuse spinner for end-game pages

--- src/components/page-game-end/page-game-end.tsx
+++ src/components/page-game-end/page-game-end.tsx
@@ -2,46 +2,43 @@ import React from 'react';
 import { observer } from 'mobx-react-lite';
 
 import { Main } from 'app/app-style';
-import { EMOJI_NEGATIVE, EMOJI_POSITIVE } from 'components/emoji/emoji';
 import { Header } from 'components/header/header';
 import { Sidebar } from 'components/sidebar/sidebar';
+import { Spinner } from 'components/spinner/spinner';
+import { SpinnerContainer, SpinnerMessage, SpinnerWrapper } from 'components/spinner/spinner-style';
 import { useGameContext } from 'services/game/context';
-import { StyledProps } from 'types/styled';
 
-import { EmojiList } from './emoji-list';
-import { FailMessage, FailWrapper, StyledGameEndPage } from './game-end-page-style';
+export const EMOJI_SVG_SPRITE = './svg/emoji.svg';
+export const EMOJI_POSITIVE = ['happy-cute', 'happy', 'in-love', 'cute', 'happy-smile'];
+export const EMOJI_NEGATIVE = ['very-sad', 'confused', 'arrogant', 'sad', 'bored'];
 
-export const GameEndPage: React.FC<StyledProps> = ({ className, children }) => {
-  return <StyledGameEndPage className={className}>{children}</StyledGameEndPage>;
-};
-
-export const WinPage: React.FC<StyledProps> = ({ className }) => {
+export const WinPage: React.FC = () => {
   return (
     <>
       <Sidebar />
       <Header />
       <Main>
-        <GameEndPage className={className}>
-          <EmojiList emojiNames={EMOJI_POSITIVE} />
-        </GameEndPage>
+        <SpinnerContainer>
+          <Spinner svgSpriteSrc={EMOJI_SVG_SPRITE} svgIcons={EMOJI_POSITIVE} />
+        </SpinnerContainer>
       </Main>
     </>
   );
 };
 
-export const FailPage: React.FC<StyledProps> = observer(({ className }) => {
+export const FailPage: React.FC = observer(() => {
   const game = useGameContext();
   return (
     <>
       <Sidebar />
       <Header />
       <Main>
-        <GameEndPage className={className}>
-          <FailWrapper>
-            <FailMessage>{game.mistakes} mistakes</FailMessage>
-            <EmojiList emojiNames={EMOJI_NEGATIVE} />
-          </FailWrapper>
-        </GameEndPage>
+        <SpinnerContainer>
+          <SpinnerWrapper>
+            <SpinnerMessage>{game.mistakes} mistakes</SpinnerMessage>
+            <Spinner svgSpriteSrc={EMOJI_SVG_SPRITE} svgIcons={EMOJI_NEGATIVE} />
+          </SpinnerWrapper>
+        </SpinnerContainer>
       </Main>
     </>
   );
