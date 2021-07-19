b73a8dc46837d40936ccff68f6f32897f0bf70f2
fronte-finem <fronte.finem+github@ya.ru>
2021-07-19 16:18:34 +0300

    refactor: simplify token-verifying middleware

--- server/middlewares/auth.ts
+++ server/middlewares/auth.ts
@@ -1,33 +1,20 @@
-import dotenv from 'dotenv';
 import { NextFunction, Request, Response } from 'express';
-import { verify } from 'jsonwebtoken';
 
-import { accessTokens } from '../stores/auth';
-
-dotenv.config();
-
-const JWT_SECRET = process.env.JWT_SECRET || 'not secret';
+import { verifyAccessToken } from '../services/auth';
 
 export function verifyToken(request: Request, response: Response, next: NextFunction): void {
   const token = request.headers['x-access-token'];
 
   if (!token || typeof token !== 'string') {
-    response.status(403).json({ msg: 'No token provided!' });
+    response.status(403).send('Token not provided!');
     return;
   }
 
-  if (!accessTokens.has(token)) {
-    response.status(401).json({ msg: 'Unknown token!' });
-    return;
-  }
-
-  verify(token, JWT_SECRET, (err, decoded) => {
-    if (err) {
-      accessTokens.delete(token);
-      response.status(401).json({ msg: 'Token expired or invalid!' });
-      return;
-    }
-    console.log('decoded jwt:', decoded);
+  try {
+    const jwtPayload = verifyAccessToken(token);
+    console.log('decoded jwt:', jwtPayload);
     next();
-  });
+  } catch (error) {
+    response.status(401).send('Token invalid!');
+  }
 }
