7a1bbaba1e5065f3e605375f33c0841890a763ed
fronte-finem <fronte.finem+github@ya.ru>
2021-07-20 21:05:47 +0300

    refactor: encapsulate work with preserve auth-token

--- src/services/rest-api/config.ts
+++ src/services/rest-api/config.ts
@@ -1,7 +1,34 @@
+import { CategoryClass } from '@server/models/category';
+import { WordClass } from '@server/models/word';
+
+export type CategoryDocument = CategoryClass & { _id: string };
+export type WordDocument = WordClass & { _id: string };
+
 declare const BACKEND_API: string;
-
 const BACKEND_API_URL: string = BACKEND_API;
-
 export { BACKEND_API_URL };
 
-export const AUTH_STORAGE_KEY = 'fronte-finem--efk--auth-token';
+const AUTH_STORAGE_KEY = 'fronte-finem--efk--auth-token';
+
+export class AuthTokenStore {
+  public static get(): string | null {
+    return localStorage.getItem(AUTH_STORAGE_KEY);
+  }
+
+  public static set(token: string): void {
+    localStorage.setItem(AUTH_STORAGE_KEY, token);
+  }
+
+  public static delete(): void {
+    localStorage.removeItem(AUTH_STORAGE_KEY);
+  }
+}
+
+interface AuthHeader {
+  'x-access-token'?: string;
+}
+
+export function authHeader(): AuthHeader {
+  const token = AuthTokenStore.get();
+  return token ? { 'x-access-token': token } : {};
+}
