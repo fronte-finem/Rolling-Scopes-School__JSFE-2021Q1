426fb08431953ff37980bf27f974f99a489cee83
fronte-finem <fronte.finem+github@ya.ru>
2021-07-21 18:09:01 +0300

    refactor: remove unnecessary console logs

--- server/index.ts
+++ server/index.ts
@@ -7,8 +7,6 @@ dotenv.config();
 
 const APP_PORT = process.env.PORT || '12321';
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/test';
-console.log(APP_PORT, MONGO_URI);
-if (!MONGO_URI) throw new Error('Env variable MONGO_URI unset');
 
 const mongodbOptions: mongoose.ConnectOptions = {
   useNewUrlParser: true,
@@ -20,8 +18,8 @@ const mongodbOptions: mongoose.ConnectOptions = {
 mongoose
   .connect(MONGO_URI, mongodbOptions)
   .then(() => console.log('Connected to mongo database'))
-  .catch((error) => console.log(error));
+  .catch((error) => console.log('Error on connect to mongo database', error));
 
-const logStart = (port: string) => () => console.log(`Server started on http://localhost:${port}`);
+const logStart = (port: string) => () => console.log(`Server started on port: ${port}`);
 
 app.listen(APP_PORT, logStart(APP_PORT));
